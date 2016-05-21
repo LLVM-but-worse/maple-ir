7ea4bf814cd46080095b4a6a086b02d63170876f
Bibl_ <GenerallyCool@hotmail.com>
2016-05-21 22:12:28 +0100

    broken propagator

--- src/main/java/org/rsdeob/stdlib/cfg/stat/Statement.java
+++ src/main/java/org/rsdeob/stdlib/cfg/stat/Statement.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.objectweb.asm.MethodVisitor;
+import org.rsdeob.stdlib.cfg.BasicBlock;
+import org.rsdeob.stdlib.cfg.FlowEdge;
 import org.rsdeob.stdlib.cfg.util.TabbedStringWriter;
 
 public abstract class Statement {
 	
+	private BasicBlock block;
 	private List<Statement> parents;
 	private Statement[] children;
 	private int ptr;
@@ -16,7 +19,48 @@ public abstract class Statement {
 		parents = new ArrayList<>();
 		children = new Statement[8];
 		ptr = 0;
-//		System.out.println("Creating " + getClass().getSimpleName() + "  as " + hashCode());
+	}
+	
+	public List<Statement> getPredecessors() {
+		List<Statement> blockStmts = block.getStatements();
+		int index = blockStmts.indexOf(this);
+		List<Statement> ret = new ArrayList<>();
+		if(index == 0) {
+			for(FlowEdge pe : block.getPredecessors()) {
+				BasicBlock pred = pe.src;
+				List<Statement> predStmts = pred.getStatements();
+				Statement last = predStmts.get(predStmts.size() - 1);
+				ret.add(last);
+			}
+		} else {
+			ret.add(blockStmts.get(index - 1));
+		}
+		return ret;
+	}
+	
+	public List<Statement> getSuccessors() {
+		List<Statement> blockStmts = block.getStatements();
+		int index = blockStmts.indexOf(this);
+		List<Statement> ret = new ArrayList<>();
+		if(index == (blockStmts.size()-1)) {
+			for(FlowEdge se : block.getSuccessors()) {
+				BasicBlock succ = se.dst;
+				List<Statement> succStmts = succ.getStatements();
+				Statement first = succStmts.get(0);
+				ret.add(first);
+			}
+		} else {
+			ret.add(blockStmts.get(index + 1));
+		}
+		return ret;
+	}
+	
+	public void setBlock(BasicBlock block) {
+		this.block = block;
+	}
+	
+	public BasicBlock getBlock() {
+		return block;
 	}
 	
 	public int size() {
