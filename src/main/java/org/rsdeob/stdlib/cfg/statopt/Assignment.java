0a72e66308981c3eb38ad199e40c2e743348de71
ecx86 <ecx86@users.noreply.github.com>
2016-05-22 02:09:42 -0400

    Write data flow analyzer.
    Holy crap.

--- src/main/java/org/rsdeob/stdlib/cfg/statopt/Assignment.java
+++ src/main/java/org/rsdeob/stdlib/cfg/statopt/Assignment.java
@@ -1,5 +1,6 @@
 package org.rsdeob.stdlib.cfg.statopt;
 
+import org.rsdeob.stdlib.cfg.stat.StackDumpStatement;
 import org.rsdeob.stdlib.cfg.stat.Statement;
 
 public class Assignment  { // also known as a 'copy' in academic terms
@@ -7,10 +8,18 @@ public class Assignment  { // also known as a 'copy' in academic terms
 	private final Statement rhs;
 
 	public Assignment(Variable var, Statement stmt) {
-		if (var == null)
-			throw new IllegalArgumentException("Variable cannot be null!");
+		if (var == null | stmt == null)
+			throw new IllegalArgumentException("Neither variable nor statement can be null!");
 		this.lhs = var;
 		this.rhs = stmt;
+	}
+
+	public Assignment(StackDumpStatement stmt) {
+		this(new Variable(stmt.getIndex(), stmt.isStackVariable()), stmt.getExpression());
+	}
+
+	public Variable getVariable() {
+		return lhs;
 	}
 
 	public Statement getStatement() {
@@ -19,23 +28,25 @@ public class Assignment  { // also known as a 'copy' in academic terms
 
 	@Override
 	public String toString() {
-		return lhs.toString();
+		return lhs + " = " + rhs + ";";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Assignment that = (Assignment) o;
+
+		if (!lhs.equals(that.lhs)) return false;
+		return rhs.equals(that.rhs);
+
 	}
 
 	@Override
 	public int hashCode() {
-		return 31 * lhs.hashCode() + 43 * rhs.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Assignment other = (Assignment) obj;
-		return other.hashCode() == this.hashCode();
+		int result = lhs.hashCode();
+		result = 31 * result + rhs.hashCode();
+		return result;
 	}
 }
--- src/main/java/org/rsdeob/stdlib/cfg/statopt/DataFlowState.java
+++ src/main/java/org/rsdeob/stdlib/cfg/statopt/DataFlowState.java
@@ -1,26 +1,89 @@
 package org.rsdeob.stdlib.cfg.statopt;
 
-import org.rsdeob.stdlib.cfg.BasicBlock;
-import org.rsdeob.stdlib.cfg.ControlFlowGraph;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Type;
+import org.rsdeob.stdlib.cfg.expr.Expression;
+import org.rsdeob.stdlib.cfg.stat.Statement;
+import org.rsdeob.stdlib.cfg.util.TabbedStringWriter;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 public class DataFlowState {
-	private final ControlFlowGraph cfg;
-	private final BasicBlock block;
+	public static final DataFlowExpression TOP_EXPR = new DataFlowExpression();
+	public static final DataFlowExpression BOTTOM_EXPR = new DataFlowExpression();
 
-	public final HashSet<Assignment> in;
-	public final HashSet<Assignment> out;
-	public final HashSet<Assignment> gen;
-	public final HashSet<Assignment> kill;
+	public HashMap<Variable, Assignment> in;
+	public HashMap<Variable, Assignment> out;
+	public final Set<Assignment> gen;
+	public final Set<Assignment> kill;
 
-	public DataFlowState(ControlFlowGraph cfg, BasicBlock b) {
-		this.cfg = cfg;
-		this.block = b;
+	public DataFlowState(HashSet<Assignment> gen, HashSet<Assignment> kill) {
+		in = new HashMap<>();
+		out = new HashMap<>();
+		this.gen = gen;
+		this.kill = kill;
+	}
 
-		in = new HashSet<>();
-		out = new HashSet<>();
-		gen = new HashSet<>();
-		kill = new HashSet<>();
+	public void copyToOut(Set<Assignment> copies) {
+		for (Assignment copy : copies) {
+			out.put(copy.getVariable(), copy);
+		}
+	}
+
+	public HashMap<Variable, Assignment> getGen() {
+		HashMap<Variable, Assignment> result = new HashMap<>();
+		for (Assignment copy : gen) {
+			result.put(copy.getVariable(), copy);
+		}
+		return result;
+	}
+
+	private static class DataFlowExpression extends Expression {
+		@Override
+		public String toString() {
+			return this == TOP_EXPR ? "T" : "_|_";
+		}
+
+		@Override
+		public void onChildUpdated(int ptr) {
+
+		}
+
+		@Override
+		public void toString(TabbedStringWriter printer) {
+
+		}
+
+		@Override
+		public void toCode(MethodVisitor visitor) {
+			throw new UnsupportedOperationException("TopExpression is for data flow use only");
+		}
+
+		@Override
+		public boolean canChangeFlow() {
+			return false;
+		}
+
+		@Override
+		public boolean canChangeLogic() {
+			return false;
+		}
+
+		@Override
+		public boolean isAffectedBy(Statement stmt) {
+			return false;
+		}
+
+		@Override
+		public Expression copy() {
+			throw new UnsupportedOperationException("Do not copy TopExpression; instantiate a new one instead");
+		}
+
+		@Override
+		public Type getType() {
+			throw new UnsupportedOperationException("TopExpression has no type");
+		}
 	}
 }
