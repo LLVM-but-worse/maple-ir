711821fac7d851259911ba848ee2c11aacb68970
Bibl_ <GenerallyCool@hotmail.com>
2016-05-22 12:34:42 +0100

    dominance stuff

--- src/main/java/org/rsdeob/stdlib/cfg/ControlFlowGraphDeobfuscator.java
+++ src/main/java/org/rsdeob/stdlib/cfg/ControlFlowGraphDeobfuscator.java
@@ -2,15 +2,8 @@ package org.rsdeob.stdlib.cfg;
 
 import static org.objectweb.asm.Opcodes.GOTO;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.Stack;
 
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.LabelNode;
@@ -620,6 +613,110 @@ public class ControlFlowGraphDeobfuscator {
 				return entryNodes.get(0);
 
 			throw new IllegalStateException("couldn't find first block for supernode");
+		}
+	}
+	
+	public static class SuperNode {
+		public final BasicBlock entry;
+		private final int hashCode;
+		public final List<BasicBlock> vertices;
+		public final List<SuperNode> successors;
+		public final List<BasicBlock> predecessors;
+
+		public SuperNode(BasicBlock entry) {
+			this.entry = entry;
+			hashCode = entry.hashCode();
+			vertices = new ArrayList<>();
+			successors = new ArrayList<>();
+			predecessors = new ArrayList<>();
+			vertices.add(entry);
+		}
+		
+		public void addEdge(SuperNode n) {
+			successors.add(n);
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("start=").append(entry.getId());
+			sb.append(",\n");
+			sb.append("   blocks=").append(GraphUtils.toBlockArray(vertices));
+			
+			sb.append(",\n   ");
+			List<BasicBlock> succs = new ArrayList<>();
+			for(SuperNode s : successors) {
+				succs.addAll(s.vertices);
+			}
+			sb.append("succs=").append(GraphUtils.toBlockArray(succs));
+			
+			sb.append(",\n   ");
+			sb.append("preds=").append(GraphUtils.toBlockArray(predecessors));
+			
+			return sb.toString();
+			
+			/* sb.append("{start=").append(entry.getId());
+			sb.append(", blocks=[");
+			ListIterator<BasicBlock> lit = vertices.listIterator();
+			while(lit.hasNext()) {
+				sb.append(lit.next().getId());
+				if(lit.hasNext()) {
+					sb.append(", ");
+				}
+			}
+
+			sb.append("], preds=[");
+			ListIterator<SuperNode> lit2 = predecessors.listIterator();
+			while(lit2.hasNext()) {
+				sb.append(lit2.next().entry.getId());
+				if(lit2.hasNext()) {
+					sb.append(", ");
+				}
+			}
+			
+			sb.append("], succs=[");
+			lit2 = successors.listIterator();
+			while(lit2.hasNext()) {
+				sb.append(lit2.next().entry.getId());
+				if(lit2.hasNext()) {
+					sb.append(", ");
+				}
+			}
+			
+			sb.append("]}"); */
+		}
+
+		@Override
+		public int hashCode() {
+//			int result = 1;
+//			result = 31 * result + ((entry == null) ? 0 : entry.hashCode());
+			// result = prime * result + ((vertices == null) ? 0 : vertices.hashCode());
+//			return result;
+//			return entry.hashCode();
+			return hashCode;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (!(obj instanceof SuperNode))
+				return false;
+			SuperNode other = (SuperNode) obj;
+			if (entry == null) {
+				if (other.entry != null)
+					return false;
+			} else if (!entry.equals(other.entry))
+				return false;
+			if (vertices == null) {
+				if (other.vertices != null)
+					return false;
+			} else if (!vertices.equals(other.vertices))
+				return false;
+			return true;
 		}
 	}
 }
---
+++ src/main/java/org/rsdeob/stdlib/cfg/algo/DominanceComputor.java
@@ -0,0 +1,192 @@
+package org.rsdeob.stdlib.cfg.algo;
+
+import java.util.*;
+
+import org.rsdeob.stdlib.cfg.BasicBlock;
+import org.rsdeob.stdlib.cfg.ControlFlowGraph;
+import org.rsdeob.stdlib.cfg.FlowEdge;
+import org.rsdeob.stdlib.collections.NullPermeableHashMap;
+import org.rsdeob.stdlib.collections.ValueCreator;
+
+// TODO: tarjans solution
+// for a block b, map.get(b) == dominators of/dom(b)
+public class DominanceComputor {
+
+	private final NullPermeableHashMap<BasicBlock, Set<BasicBlock>> dominators = 
+			new NullPermeableHashMap<>(new ValueCreator<Set<BasicBlock>>() {
+		@Override public Set<BasicBlock> create() {return new HashSet<>();}});
+	
+	private final Map<BasicBlock, Set<BasicBlock>> strictDominators = new HashMap<>();
+	private final Map<BasicBlock, BasicBlock> immediateDomintors = new HashMap<>();
+	private final Map<BasicBlock, Integer> postOrderNumbers = new HashMap<>();
+	
+	public DominanceComputor(ControlFlowGraph cfg) {
+		generate(cfg);
+	}
+	
+	public Set<BasicBlock> doms(BasicBlock b) {
+		return dominators.get(b);
+	}
+	
+	public BasicBlock idom(BasicBlock b) {
+		return immediateDomintors.get(b);
+	}
+	
+	public Set<BasicBlock> sdoms(BasicBlock b) {
+		if(strictDominators.containsKey(b)) {
+			return strictDominators.get(b);
+		} else {
+			Set<BasicBlock> sdom = new HashSet<>(doms(b));
+			sdom.remove(b);
+			strictDominators.put(b, sdom);
+			return sdom;
+		}
+	}
+	
+	private void generate2(ControlFlowGraph cfg) {
+		Set<BasicBlock> eset = new HashSet<>();
+		BasicBlock entry = cfg.getEntry();
+		eset.add(entry);
+		dominators.put(entry, eset);
+		
+		List<BasicBlock> rpo = new ArrayList<>(new DepthFirstSorter(cfg).getReversePostOrder());
+		int i = 0;
+		for(BasicBlock b : rpo) {
+			postOrderNumbers.put(b, i++);
+		}
+		rpo.remove(entry);
+		
+		boolean changed = true;
+		while(changed) {
+			changed = false;
+			
+			for(BasicBlock b : rpo) {
+				BasicBlock oldIdom = immediateDomintors.get(b);
+				BasicBlock idom = null;
+				
+				Iterator<FlowEdge> it = b.getPredecessors().iterator();
+				while(it.hasNext()) {
+					FlowEdge fe = it.next();
+					BasicBlock p = fe.src;
+					
+					if(idom(p) != null) {
+						idom = intersect(idom, p);
+						if(idom == null) {
+							break;
+						}
+					}
+				}
+				
+				if(idom == null) {
+					idom = b;
+				}
+				
+				if(oldIdom != idom) {
+					immediateDomintors.put(b, idom);
+					changed = true;
+				}
+			}
+			
+			if(!changed) {
+				break;
+			}
+		}
+	}
+	
+	private BasicBlock intersect(BasicBlock n1, BasicBlock n2) {
+		if(n1 == null) {
+			return n2;
+		} else if(n2 == null) {
+			return n1;
+		}
+		
+		int f1 = postOrderNumbers.get(n1);
+		int f2 = postOrderNumbers.get(n1);
+		
+		while(f1 != f2) {
+			if(f1 > f2) {
+				BasicBlock f3 = n1;
+				n1 = immediateDomintors.get(n1);
+				if(f3 == n1) {
+					return null;
+				}
+				f1 = postOrderNumbers.get(n1);
+			} else {
+				BasicBlock f3 = n2;
+				n2 = immediateDomintors.get(n2);
+				if(f3 == n2) {
+					return null;
+				}
+				f2 = postOrderNumbers.get(n2);
+			}
+		}
+		
+		
+		return n1;
+	}
+	
+	private void generate(ControlFlowGraph cfg) {
+		Set<BasicBlock> blocks = new HashSet<BasicBlock>(cfg.blocks());
+		for(BasicBlock b : blocks) {
+			dominators.put(b, blocks);
+		}
+		
+		LinkedList<BasicBlock> queue = new LinkedList<>();
+		queue.add(cfg.getEntry());
+		
+		while(!queue.isEmpty()) {
+			BasicBlock b = queue.pop();
+			
+			Set<BasicBlock> n = new HashSet<>();
+			for(FlowEdge fe : b.getPredecessors()) {
+				BasicBlock pred = fe.src;
+				if(n.isEmpty()) {
+					n.addAll(dominators.get(pred));
+				} else {
+					n.retainAll(dominators.get(pred));
+				}
+			}
+			n.add(b);
+			
+			Set<BasicBlock> old = dominators.get(b);
+			
+			if(!old.equals(n)) {
+				dominators.put(b, n);
+				for(FlowEdge se : b.getSuccessors()) {
+					BasicBlock succ = se.dst;
+					if(!queue.contains(succ)) {
+						queue.addLast(succ);
+					}
+				}
+			}
+		}
+	}
+	
+	public static Map<BasicBlock, Set<BasicBlock>> computeFrontiers(ControlFlowGraph cfg, Map<BasicBlock, Set<BasicBlock>> dominators) {
+		LinkedList<BasicBlock> queue = new LinkedList<>();
+		LinkedList<BasicBlock> tree = new LinkedList<>();
+		queue.add(cfg.getEntry());
+		
+		while(!queue.isEmpty()) {
+			BasicBlock c = queue.removeLast();
+			tree.add(c);
+
+			for(BasicBlock d : dominators.get(c)) {
+				tree.add(d);
+				queue.addFirst(d);
+			}
+		}
+		
+		while(!tree.isEmpty()) {
+			BasicBlock n = tree.removeLast();
+			BasicBlock s = null;
+			
+			for(FlowEdge se : n.getSuccessors()) {
+				BasicBlock succ = se.dst;
+				
+			}
+		}
+	}
+	
+	
+}
