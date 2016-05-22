72da19c0282fbdd599e33a1df5c434179009f949
rcx <rcx@users.noreply.github.com>
2016-05-21 21:55:18 -0400

    Refactor data flow structures

---
+++ src/main/java/org/rsdeob/stdlib/cfg/statopt/Variable.java
@@ -0,0 +1,41 @@
+package org.rsdeob.stdlib.cfg.statopt;
+
+public class Variable {
+	private final int index;
+	private final boolean stackVar;
+
+	public Variable(int index, boolean stackVar) {
+		this.index = index;
+		this.stackVar = stackVar;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public boolean isStackVar() {
+		return stackVar;
+	}
+
+	String createVarName() {
+		return (stackVar ? "svar" : "lvar") + index;
+	}
+
+	@Override
+	public int hashCode() {
+		return 37 * index + 41 * (stackVar? 's' : 'l');
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Variable other = (Variable) obj;
+		return other.hashCode() == this.hashCode();
+	}
+}
+
