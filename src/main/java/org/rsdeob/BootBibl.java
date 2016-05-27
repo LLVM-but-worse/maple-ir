4b2ba6c237839cce9f4950648bd7c5ea86de3ded
Bibl_ <GenerallyCool@hotmail.com>
2016-05-27 21:17:00 +0100

    added entries to sgraph

--- src/main/java/org/rsdeob/BootBibl.java
+++ src/main/java/org/rsdeob/BootBibl.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.jar.JarOutputStream;
 
 import org.objectweb.asm.ClassReader;
@@ -29,9 +28,8 @@ import org.rsdeob.stdlib.cfg.ControlFlowGraphBuilder;
 import org.rsdeob.stdlib.cfg.ControlFlowGraphDeobfuscator;
 import org.rsdeob.stdlib.cfg.ir.RootStatement;
 import org.rsdeob.stdlib.cfg.ir.StatementGenerator;
-import org.rsdeob.stdlib.cfg.ir.stat.CopyVarStatement;
-import org.rsdeob.stdlib.cfg.ir.transform.DataFlowAnalyzer;
-import org.rsdeob.stdlib.cfg.ir.transform.DataFlowState;
+import org.rsdeob.stdlib.cfg.ir.StatementGraphBuilder;
+import org.rsdeob.stdlib.cfg.ir.transform.BiblPropagator;
 import org.rsdeob.stdlib.cfg.util.GraphUtils;
 import org.rsdeob.stdlib.collections.NodeTable;
 import org.rsdeob.stdlib.collections.graph.flow.TarjanDominanceComputor;
@@ -86,21 +84,23 @@ public class BootBibl implements Opcodes {
 			System.out.println(root);
 			System.out.println();
 
-			DataFlowAnalyzer dfa = new DataFlowAnalyzer(cfg, false);
-			Map<BasicBlock, DataFlowState> df = dfa.computeForward();
-			System.out.println("Data flow for " + m + ":");
-			for (BasicBlock b : df.keySet()) {
-				DataFlowState state = df.get(b);
-				System.out.println("Data flow for block " + b.getId() + ":");
-				System.out.println("In: ");
-				for (CopyVarStatement copy : state.in.values())
-					System.out.println("  " + copy);
-				System.out.println();
-
-				System.out.println("Out: ");
-				for (CopyVarStatement copy : state.out.values())
-					System.out.println("  " + copy);
-				System.out.println();
+			BiblPropagator prop = new BiblPropagator(new StatementGraphBuilder().create(cfg), m);
+			
+//			DataFlowAnalyzer dfa = new DataFlowAnalyzer(cfg, false);
+//			Map<BasicBlock, DataFlowState> df = dfa.computeForward();
+//			System.out.println("Data flow for " + m + ":");
+//			for (BasicBlock b : df.keySet()) {
+//				DataFlowState state = df.get(b);
+//				System.out.println("Data flow for block " + b.getId() + ":");
+//				System.out.println("In: ");
+//				for (CopyVarStatement copy : state.in.values())
+//					System.out.println("  " + copy);
+//				System.out.println();
+//
+//				System.out.println("Out: ");
+//				for (CopyVarStatement copy : state.out.values())
+//					System.out.println("  " + copy);
+//				System.out.println();
 
 //				System.out.println("Gen: ");
 //				for (CopyVarStatement copy : state.gen)
@@ -111,7 +111,7 @@ public class BootBibl implements Opcodes {
 //				for (CopyVarStatement copy : state.kill)
 //					System.out.println("  " + copy);
 //				System.out.println();
-			}
+//			}
 			
 			TarjanDominanceComputor<BasicBlock> doms = new TarjanDominanceComputor<>(cfg);
 			for(BasicBlock b : cfg.vertices()) {
