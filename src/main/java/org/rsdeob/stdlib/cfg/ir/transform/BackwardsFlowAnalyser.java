0a939d67241e771c4146ebd51c09a3c38bfc8a2c
ecx86 <ecx86@users.noreply.github.com>
2016-05-29 13:53:13 -0400

    Move transform1 to transform

---
+++ src/main/java/org/rsdeob/stdlib/cfg/ir/transform/BackwardsFlowAnalyser.java
@@ -0,0 +1,99 @@
+package org.rsdeob.stdlib.cfg.ir.transform;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+
+import org.rsdeob.stdlib.collections.graph.FastGraphEdge;
+import org.rsdeob.stdlib.collections.graph.FastGraphVertex;
+import org.rsdeob.stdlib.collections.graph.flow.FlowGraph;
+
+public abstract class BackwardsFlowAnalyser<N extends FastGraphVertex, E extends FastGraphEdge<N>, S> extends DataAnalyser<N, E, S> {
+
+	public BackwardsFlowAnalyser(FlowGraph<N, E> graph) {
+		super(graph, new LinkedList<>());
+	}
+
+	@Override
+	protected void init() {
+		// since this is backwards analysis, we
+		// set the initial flow states after the
+		// exit points of the graph.
+		
+		// to increase efficiency, instead of
+		// calling super.init(), we compute the
+		// exits of the graph while inserting
+		// the default flow states into our tables.
+		
+		for(N n : graph.vertices()) {
+			queue.add(n);
+			in.put(n, newState());
+			out.put(n, newState());
+			
+			if(graph.getEdges(n).size() == 0) {
+				out.put(n, newEntryState());
+			}
+		}
+	}
+	
+	@Override
+	protected void processQueue() {		
+		while(!queue.isEmpty()) {
+			N n = queue.iterator().next();
+			queue.remove(n);
+
+			// stored for checking if a change of state
+			// happens during the analysis of this
+			// instruction. (in because it's backwards).
+			S oldIn = newState();
+			S currentIn = in.get(n);
+			copy(currentIn, oldIn);
+			
+			S currentOut = out.get(n);
+			Set<E> succs = graph.getEdges(n);
+			
+			if(succs.size() == 1) {
+				N succ = succs.iterator().next().dst;
+				S succIn = in.get(succ);
+				copy(succIn, currentOut);
+			} else if(succs.size() > 1) {
+				Iterator<E> it = succs.iterator();
+				
+				N firstSucc = it.next().dst;
+				copy(in.get(firstSucc), currentOut);
+				
+				while(it.hasNext()) {
+					S merging = in.get(it.next().dst);
+					merge(currentOut, merging);
+				}
+			}
+			
+			propagate(n, currentOut, currentIn);
+			
+			// if there was a change, queue the predecessors.
+			if(!equals(currentIn, oldIn)) {
+				for(E e : graph.getReverseEdges(n)) {
+					queue.add(e.src);
+				}
+			}
+		}
+	}
+
+	@Override
+	protected abstract S newState();
+
+	@Override
+	protected abstract S newEntryState();
+
+	@Override
+	protected abstract void merge(S in1, S in2, S out);
+	
+	@Override
+	protected abstract void copy(S src, S dst);
+	
+	@Override
+	protected abstract boolean equals(S s1, S s2);
+	
+	@Override
+	protected abstract void propagate(N n, S in, S out);
+}
