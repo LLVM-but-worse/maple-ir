3e84ae13cf374542d01b452ff844e0bb1a771307
Bibl_ <GenerallyCool@hotmail.com>
2016-05-30 15:41:11 +0100

    bug in liveness analyser

--- src/main/java/org/rsdeob/LivenessTest.java
+++ src/main/java/org/rsdeob/LivenessTest.java
@@ -3,19 +3,74 @@ package org.rsdeob;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.rsdeob.stdlib.cfg.BasicBlock;
+import org.rsdeob.stdlib.cfg.ControlFlowGraph;
+import org.rsdeob.stdlib.cfg.ControlFlowGraphBuilder;
+import org.rsdeob.stdlib.cfg.ir.RootStatement;
 import org.rsdeob.stdlib.cfg.ir.StatementBuilder;
+import org.rsdeob.stdlib.cfg.ir.StatementGenerator;
+import org.rsdeob.stdlib.cfg.ir.StatementGraph;
+import org.rsdeob.stdlib.cfg.ir.StatementGraphBuilder;
 import org.rsdeob.stdlib.cfg.ir.expr.ArithmeticExpression.Operator;
 import org.rsdeob.stdlib.cfg.ir.expr.Expression;
 import org.rsdeob.stdlib.cfg.ir.expr.VarExpression;
 import org.rsdeob.stdlib.cfg.ir.stat.ConditionalJumpStatement.ComparisonType;
 import org.rsdeob.stdlib.cfg.ir.stat.Statement;
+import org.rsdeob.stdlib.cfg.ir.transform.impl.LivenessAnalyser;
+import org.rsdeob.stdlib.cfg.util.ControlFlowGraphDeobfuscator;
+import org.rsdeob.stdlib.cfg.util.GraphUtils;
 import org.rsdeob.stdlib.cfg.util.TabbedStringWriter;
 
 public class LivenessTest {
 
-	public static void main(String[] args) throws Exception {		
+	public static void main(String[] args) throws Exception {
+		ClassNode cn = new ClassNode();
+		ClassReader cr = new ClassReader(LivenessTest.class.getCanonicalName());
+		cr.accept(cn, 0);
+		
+		for(MethodNode m : cn.methods) {
+			if(m.name.startsWith("test1")) {
+				ControlFlowGraphBuilder cfgbuilder = new ControlFlowGraphBuilder(m);
+				ControlFlowGraph cfg = cfgbuilder.build();
+				
+				ControlFlowGraphDeobfuscator deobber = new ControlFlowGraphDeobfuscator();
+				List<BasicBlock> blocks = deobber.deobfuscate(cfg);
+				GraphUtils.naturaliseGraph(cfg, blocks);
+				
+				StatementGenerator generator = new StatementGenerator(cfg);
+				generator.init(m.maxLocals);
+				generator.createExpressions();
+				RootStatement root = generator.buildRoot();
+				
+				StatementGraph sgraph = StatementGraphBuilder.create(cfg);
+				System.out.println("Processing " + m);
+				System.out.println(cfg);
+				System.out.println(root);
+				System.out.println();
+				
+				LivenessAnalyser la = new LivenessAnalyser(sgraph);
+				la.run();
+				
+			}
+		}
+	}
+	
+	void test1() {
+		int x = 0;
+		int z = 10;
+		while(x <= 10) {
+			x++;
+			z += x;
+		}
+		System.out.println(x);
+	}
+	
+	public static void main1(String[] args) throws Exception {		
 		VarExpression x = new VarExpression(0, Type.INT_TYPE) {
 			@Override
 			public void toString(TabbedStringWriter printer) {
@@ -40,7 +95,5 @@ public class LivenessTest {
 		}
 		
 		b.add(b.call(Opcodes.INVOKESTATIC, "test", "use", "(I)V", new Expression[]{x}));
-		
-		System.out.println(b.getRoot());
 	}
 }
