15987ea828aed4f298c2498d58bbdf7a9916fe29
rollsafe <rollsafe@users.noreply.github.com>
2016-06-03 01:25:16 -0400

    Organize

--- src/main/java/org/rsdeob/BootEcx.java
+++ src/main/java/org/rsdeob/BootEcx.java
@@ -5,28 +5,37 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.rsdeob.byteio.CompleteResolvingJarDumper;
+import org.rsdeob.deobimpl.ControlFlowFixerPhase;
+import org.rsdeob.deobimpl.DummyMethodPhase;
+import org.rsdeob.deobimpl.RTECatchBlockRemoverPhase;
+import org.rsdeob.deobimpl.UnusedFieldsPhase;
+import org.rsdeob.stdlib.IContext;
+import org.rsdeob.stdlib.cfg.BasicBlock;
 import org.rsdeob.stdlib.cfg.ControlFlowGraph;
 import org.rsdeob.stdlib.cfg.ControlFlowGraphBuilder;
 import org.rsdeob.stdlib.cfg.ir.RootStatement;
 import org.rsdeob.stdlib.cfg.ir.StatementGenerator;
-import org.rsdeob.stdlib.cfg.ir.expr.Expression;
-import org.rsdeob.stdlib.cfg.ir.stat.CopyVarStatement;
-import org.rsdeob.stdlib.cfg.ir.stat.Statement;
-import org.rsdeob.stdlib.cfg.ir.exprtransform.DataFlowAnalyzer;
-import org.rsdeob.stdlib.cfg.ir.exprtransform.DataFlowState;
-import org.rsdeob.stdlib.cfg.ir.exprtransform.ExpressionEvaluator;
+import org.rsdeob.stdlib.cfg.ir.StatementGraph;
+import org.rsdeob.stdlib.cfg.ir.StatementGraphBuilder;
+import org.rsdeob.stdlib.cfg.util.ControlFlowGraphDeobfuscator;
+import org.rsdeob.stdlib.cfg.util.GraphUtils;
+import org.rsdeob.stdlib.collections.NodeTable;
+import org.rsdeob.stdlib.deob.IPhase;
+import org.topdank.byteengineer.commons.data.JarInfo;
+import org.topdank.byteio.in.SingleJarDownloader;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.util.HashMap;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
+import java.util.jar.JarOutputStream;
 
 @SuppressWarnings("Duplicates")
 public class BootEcx implements Opcodes {
 	public static void main(String[] args) throws Exception {
-		InputStream i = new FileInputStream(new File("res/Test.class"));
+		InputStream i = new FileInputStream(new File("res/a.class"));
 		ClassReader cr = new ClassReader(i);
 		ClassNode cn = new ClassNode();
 		cr.accept(cn, 0);
@@ -35,7 +44,7 @@ public class BootEcx implements Opcodes {
 		while(it.hasNext()) {
 			MethodNode m = it.next();
 
-			if(m.toString().contains("<init>")) {
+			if(!m.toString().equals("a/a/f/a.<init>()V")) {
 				continue;
 			}
 
@@ -46,6 +55,14 @@ public class BootEcx implements Opcodes {
 
 			ControlFlowGraphBuilder builder = new ControlFlowGraphBuilder(m);
 			ControlFlowGraph cfg = builder.build();
+
+			ControlFlowGraphDeobfuscator deobber = new ControlFlowGraphDeobfuscator();
+			List<BasicBlock> blocks = deobber.deobfuscate(cfg);
+			deobber.removeEmptyBlocks(cfg, blocks);
+			GraphUtils.naturaliseGraph(cfg, blocks);
+
+//			GraphUtils.output(cfg, blocks, GRAPH_FOLDER, "");
+//			System.out.println(cfg);
 
 			System.out.println("Execution log of " + m + ":");
 			StatementGenerator gen = new StatementGenerator(cfg);
@@ -61,45 +78,207 @@ public class BootEcx implements Opcodes {
 			System.out.println(root);
 			System.out.println();
 
-//			System.out.println("Sg:");
-//			System.out.println(StatementGraphBuilder.create(cfg));
-//			System.out.println();
+			StatementGraph sgraph = StatementGraphBuilder.create(cfg);
+			LivenessTest.simplify(root, sgraph, m);
 
-			DataFlowAnalyzer dfa = new DataFlowAnalyzer(cfg);
-			HashMap<Statement, DataFlowState> df = dfa.compute();
-			System.out.println("Data flow for " + m + ":");
-			for (Statement b : df.keySet()) {
-				DataFlowState state = df.get(b);
-				System.out.println(b + " (" + b.getId() + ") :");
-				System.out.println("In: ");
-				for (CopyVarStatement copy : state.in.values())
-					System.out.println("  " + copy);
-				System.out.println();
+			System.out.println("IR representation of " + m + ":");
+			System.out.println(root);
+			System.out.println();
 
-				System.out.println("Out: ");
-				for (CopyVarStatement copy : state.out.values())
-					System.out.println("  " + copy);
-
-				if (b instanceof CopyVarStatement) {
-					Expression rhs = ((CopyVarStatement) b).getExpression();
-					Expression evaluated = ExpressionEvaluator.evaluate(rhs, state.in);
-					System.out.println("Evaluated: " + evaluated + " (constant=" + ExpressionEvaluator.isConstant(evaluated) + ")");
-				}
-
-				System.out.println();
-			}
 
 			System.out.println("End of processing log for " + m);
 			System.out.println("============================================================");
 			System.out.println("============================================================\n\n");
 			break;
 		}
-		
+
 		ClassWriter clazz = new ClassWriter(0);
 		cn.accept(clazz);
 		byte[] saved = clazz.toByteArray();
 		FileOutputStream out = new FileOutputStream(new File("out/testclass.class"));
 		out.write(saved, 0, saved.length);
 		out.close();
+	}
+
+	public void t3(Object o) {
+		synchronized (o == null ? this : o) {
+			System.out.println(o);
+			System.out.println(this);
+			t2(5, 7, 0);
+		}
+	}
+
+	public void t2(int i, int j, int k) {
+		t1(5);
+		if(i > 0) {
+			t2(i - 1, j, k);
+		}
+		t1(16);
+	}
+
+	public void t1(int i) {
+		System.out.println(i);
+		System.out.println(i + " hi " + i);
+	}
+
+	public void test4() {
+		BootBibl newBoot = new BootBibl();
+		newBoot.DVAL += new C2().FVAL;
+		new C2().FVAL = (float) newBoot.DVAL;
+
+		System.out.println(newBoot);
+	}
+
+	public void test6() {
+		for(int i=0; i < 3; i++) {
+			test5();
+		}
+	}
+
+	public void test5() {
+		BootBibl boot = new BootBibl();
+		boot.DVAL = 5;
+	}
+
+	static class C2 extends BootBibl {
+	}
+
+	double DVAL = 5D;
+	float FVAL = 10F;
+	String SVAL = "";
+
+	public void test3() {
+		double d = DVAL * 847545D;
+		float f = FVAL * 8573845743F;
+		double c = 0;
+		if(d > f) {
+			c = (d + f);
+		} else {
+			c = (d - f);
+		}
+
+		System.out.println(c);
+	}
+
+	public void test2(int i, int j, int k) {
+		int s = 283472 * i;
+		int d = j * 4334857;
+		int f = 345345 * (34784 * k);
+
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println(f);
+	}
+
+	public void test(int i) {
+		System.out.println("k " + i);
+	}
+
+	public float ternTest2(int i) {
+		return normalReturnTest(i == 8 ? 5 : 10);
+	}
+
+	public float normalReturnTest(int i) {
+		if(i == 1) {
+			return 5F;
+		} else {
+			return 10F;
+		}
+	}
+
+	public float ternTest(int i) {
+		return i == 1 ? normalReturnTest(i) : normalReturnTest(i);
+	}
+
+	public static void main111(String[] args) throws Exception {
+//		Thread.sleep(15000);
+		System.out.println("starting");
+		IPhase[] phases = loadPhases();
+		if(phases.length <= 0) {
+			System.err.println("No passes to complete.");
+			return;
+		}
+
+		int rev = 107;
+		if(args.length > 0) {
+			rev = Integer.parseInt(args[0]);
+		}
+
+		NodeTable<ClassNode> nt = new NodeTable<ClassNode>();
+		SingleJarDownloader<ClassNode> dl = new SingleJarDownloader<ClassNode>(new JarInfo(new File(String.format("res/gamepack%s.jar", rev))));
+		dl.download();
+		nt.putAll(dl.getJarContents().getClassContents().namedMap());
+		IContext cxt = new IContext() {
+			@Override
+			public NodeTable<ClassNode> getNodes() {
+				return nt;
+			}
+
+			@Override
+			public ControlFlowGraph createControlFlowGraph(MethodNode m) {
+				return ControlFlowGraphBuilder.create(m);
+			}
+		};
+
+		List<IPhase> completed = new ArrayList<IPhase>();
+		IPhase prev = null;
+		for(IPhase p : phases) {
+			System.out.println("Running " + p.getId());
+			try {
+				p.accept(cxt, prev, Collections.unmodifiableList(completed));
+				prev = p;
+				completed.add(p);
+				System.out.println("Completed " + p.getId());
+			} catch(RuntimeException e) {
+				System.err.println("Error: " + p.getId());
+				System.err.flush();
+				e.printStackTrace(System.err);
+				prev = null;
+			}
+		}
+
+		CompleteResolvingJarDumper dumper = new CompleteResolvingJarDumper(dl.getJarContents()) {
+			@Override
+			public int dumpResource(JarOutputStream out, String name, byte[] file) throws IOException {
+				if(name.startsWith("META-INF")) {
+					return 0;
+				} else {
+					return super.dumpResource(out, name, file);
+				}
+			}
+		};
+		File outFile = new File(String.format("out/%d/%d.jar", rev, rev));
+		outFile.mkdirs();
+		dumper.dump(outFile);
+
+
+//		URLClassLoader cl = new URLClassLoader(new URL[]{outFile.toURI().toURL()});
+//		Class<?> c = cl.loadClass("aa");
+//		Constructor<?> cs = c.getDeclaredConstructors()[0];
+//		cs.setAccessible(true);
+//		Object o = cs.newInstance();
+//		Method m = c.getDeclaredMethod("h", new Class<?>[]{int.class, int.class, byte.class});
+//		m.setAccessible(true);
+//		m.invoke(null, new Object[]{1, 1, 1});
+//		System.out.println(o);
+		Runtime.getRuntime().exec(new String[]{"java", "-jar", "F:/bcv.jar", outFile.getAbsolutePath()});
+	}
+
+	private static IPhase[] loadPhases() {
+		return new IPhase[] { new DummyMethodPhase(), new UnusedFieldsPhase(), new RTECatchBlockRemoverPhase(),  /* new OpaquePredicateRemoverPhase(),*/ new ControlFlowFixerPhase() /*new ConstantComparisonReordererPhase(),new EmptyParameterFixerPhase(), new ConstantOperationReordererPhase()*/ };
+	}
+
+	public void cfg(int x, int y) {
+		int s = 0;
+		int d = 0;
+		while(x < y) {
+			x+=3;
+			y+=2;
+			if(x + y < 100) {
+				s+= (x + y);
+			} else {
+				d+= (x + y);
+			}
+		}
 	}
 }
--- src/main/java/org/rsdeob/stdlib/cfg/ir/transform/impl/ValuePropagator.java
+++ src/main/java/org/rsdeob/stdlib/cfg/ir/transform/impl/ValuePropagator.java
@@ -1,124 +1,18 @@
 package org.rsdeob.stdlib.cfg.ir.transform.impl;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.rsdeob.stdlib.cfg.edge.FlowEdge;
-import org.rsdeob.stdlib.cfg.ir.RootStatement;
 import org.rsdeob.stdlib.cfg.ir.StatementGraph;
 import org.rsdeob.stdlib.cfg.ir.StatementVisitor;
-import org.rsdeob.stdlib.cfg.ir.expr.ConstantExpression;
 import org.rsdeob.stdlib.cfg.ir.expr.Expression;
 import org.rsdeob.stdlib.cfg.ir.expr.VarExpression;
 import org.rsdeob.stdlib.cfg.ir.exprtransform.DataFlowState;
-import org.rsdeob.stdlib.cfg.ir.exprtransform.ExpressionEvaluator;
 import org.rsdeob.stdlib.cfg.ir.stat.CopyVarStatement;
 import org.rsdeob.stdlib.cfg.ir.stat.Statement;
+import org.rsdeob.stdlib.cfg.util.ExpressionEvaluator;
+
+import java.util.Map;
+import java.util.Set;
 
 public class ValuePropagator {
-
-	public static int propagateDefinitions1(RootStatement root, StatementGraph graph, DefinitionAnalyser da) {
-		AtomicInteger propagated = new AtomicInteger();
-		
-		for (Statement stmt : graph.vertices()) {
-			Map<String, Set<CopyVarStatement>> in = da.in(stmt);
-
-			StatementVisitor impl = new StatementVisitor(stmt) {
-				@Override
-				public Statement visit(Statement s) {
-					if (s instanceof VarExpression) {
-						Set<CopyVarStatement> defs = in.get(s.toString());
-
-						if (defs.size() == 1) {
-							CopyVarStatement def = defs.iterator().next();
-							Expression rhs = def.getExpression();
-
-							if (rhs instanceof ConstantExpression) {
-								// constant propagation is easy
-								propagated.incrementAndGet();
-								return rhs.copy();
-
-							} else {
-								AtomicBoolean canProp = new AtomicBoolean(true);
-
-								if (rhs instanceof VarExpression) {
-									// hold onto your seat...
-
-									Set<CopyVarStatement> rhsDefs = in.get(rhs.toString());
-									if (rhsDefs.size() == 1) {
-										CopyVarStatement rhsDef = rhsDefs.iterator().next();
-										Expression tail = rhsDef.getExpression();
-
-										if (tail instanceof VarExpression) {
-
-											 System.out.println("Statement: " + stmt);
-											 System.out.println(" rdef of " + s + ": " + def);
-											 System.out.println(" rhs: " + rhs);
-											 System.out.println(" rdef of " + rhs + ": " + rhsDef);
-											 System.out.println(" tail: " + tail);
-
-											// tail must not be redefined between
-											// def and rhsDef
-
-											// one way to calculate this is to check
-											// this is:
-											// function defs(var, stmt)
-											// if(defs(tail, def) == defs(tail, rhsDef)) then propagate
-											// ... or not.
-
-											StatementVisitor vis = new StatementVisitor(root) {
-												boolean start = false;
-
-												@Override
-												public Statement visit(Statement s) {
-													if (!start) {
-														if (s == tail) {
-															start = true;
-														}
-														return s;
-													}
-
-													// check only until the next block boundary.
-													Set<FlowEdge<Statement>> pes = graph.getReverseEdges(s);
-													if (pes != null && pes.size() > 1) {
-														_break();
-														return s;
-													}
-
-													if (s instanceof CopyVarStatement) {
-														CopyVarStatement cp = (CopyVarStatement) s;
-														VarExpression cpv = cp.getVariable();
-														if (cpv.toString().equals(tail.toString())) {
-															canProp.set(false);
-														}
-													}
-
-													return s;
-												}
-											};
-											vis.visit();
-
-											if (canProp.get()) {
-												propagated.incrementAndGet();
-												return rhs.copy();
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-					return s;
-				}
-			};
-			impl.visit();
-		}
-		
-		return propagated.get();
-	}
-
 	private static void propagateDefinitions(StatementGraph graph, DefinitionAnalyser da) {
 		for (Statement stmt : graph.vertices()) {
 			Map<String, Set<CopyVarStatement>> in = da.in(stmt);
@@ -139,23 +33,6 @@ public class ValuePropagator {
 							if (ExpressionEvaluator.isConstant(evaluated)) {
 								int d = getDepth();
 								getCurrent(d).overwrite(evaluated.copy(), getCurrentPtr(d));
-							}
-
-							if (defValue instanceof VarExpression) {
-								// must check that the defValue also has 1 def here
-								Set<CopyVarStatement> exprDefs = da.in(stmt).get(defValue.toString());
-								System.out.println("redundant " + stmt + "   exprdefs  " + exprDefs);
-								int d = getDepth();
-								getCurrent(d).overwrite(defValue.copy(), getCurrentPtr(d));
-							} else {
-								if (name.startsWith("svar")) {
-									// not live out, but live in, so
-									// if(!la.out(stmt).get(name)) {
-									// System.out.println("not live " + stmt);
-									// int d = getDepth();
-									// getCurrent(d).overwrite(def.copy(), getCurrentPtr(d));
-									// }
-								}
 							}
 						}
 					}
