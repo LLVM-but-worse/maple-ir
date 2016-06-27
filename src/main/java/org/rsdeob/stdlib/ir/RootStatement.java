9b28f19bae6bb763b5fb9199494d8ea63d9d775f
Bibl_ <GenerallyCool@hotmail.com>
2016-06-28 00:44:26 +0100

    Removed BasicBlock coupling from RootStatement

--- src/main/java/org/rsdeob/stdlib/ir/RootStatement.java
+++ src/main/java/org/rsdeob/stdlib/ir/RootStatement.java
@@ -1,19 +1,10 @@
 package org.rsdeob.stdlib.ir;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.tree.MethodNode;
-import org.rsdeob.stdlib.cfg.BasicBlock;
 import org.rsdeob.stdlib.cfg.util.TabbedStringWriter;
-import org.rsdeob.stdlib.collections.graph.flow.ExceptionRange;
 import org.rsdeob.stdlib.ir.expr.VarExpression;
-import org.rsdeob.stdlib.ir.header.HeaderStatement;
 import org.rsdeob.stdlib.ir.header.StatementHeaderStatement;
 import org.rsdeob.stdlib.ir.stat.Statement;
 import org.rsdeob.stdlib.ir.transform.impl.CodeAnalytics;
@@ -21,13 +12,11 @@ import org.rsdeob.stdlib.ir.transform.impl.CodeAnalytics;
 public class RootStatement extends Statement {
 
 	private final LocalsHandler locals;
-	private final Map<BasicBlock, HeaderStatement> headers;
 	private final AtomicInteger maxLocals;
 	
 	public RootStatement(int maxL) {
 		maxLocals = new AtomicInteger(maxL + 1);
 		locals = new LocalsHandler(maxLocals);
-		headers = new HashMap<>();
 	}
 
 	public void updateBase() {
@@ -45,10 +34,6 @@ public class RootStatement extends Statement {
 		}.visit();
 		maxLocals.set(max.get() + 1);
 		System.out.println("New maxlocals: " + maxLocals.get());
-	}
-	
-	public Map<BasicBlock, HeaderStatement> getHeaders() {
-		return headers;
 	}
 	
 	public LocalsHandler getLocals() {
@@ -79,46 +64,9 @@ public class RootStatement extends Statement {
 
 	@Override
 	public void toCode(MethodVisitor visitor, CodeAnalytics analytics) {
-		for(HeaderStatement hs : headers.values()) {
-			hs.resetLabel();
-		}
 		for (int addr = 0; read(addr) != null; addr++) {
 			read(addr).toCode(visitor, analytics);
 		}
-	}
-
-	public void dump(MethodNode m, CodeAnalytics analytics) {
-		m.visitCode();
-		m.instructions.clear();
-		m.tryCatchBlocks.clear();
-		updateBase();
-		locals.pack(this);
-		toCode(m, analytics);
-		for(ExceptionRange<BasicBlock> er : analytics.blockGraph.getRanges()) {
-			String type = null;
-			Set<String> typeSet = er.getTypes();
-			if(typeSet.size() == 0 || typeSet.size() > 1) {
-				// TODO: fix base exception
-				type = Throwable.class.getCanonicalName().replace(".", "/");
-			} else {
-				// size == 1
-				type = typeSet.iterator().next();
-			}
-			List<BasicBlock> range = er.get();
-			Label start = headers.get(range.get(0)).getLabel();
-			Label end = null;
-			BasicBlock endBlock = range.get(range.size() - 1);
-			BasicBlock im = endBlock.getImmediate();
-			if(im == null) {
-				// end of code?
-				throw new RuntimeException(m.toString());
-			} else {
-				end = headers.get(im).getLabel();
-			}
-			Label handler = headers.get(er.getHandler()).getLabel();
-			m.visitTryCatchBlock(start, end, handler, type);
-		}
-		m.visitEnd();
 	}
 
 	@Override
