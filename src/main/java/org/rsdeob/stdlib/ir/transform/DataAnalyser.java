e898533c0a4248f4659a2136f164feada59e842c
rollsafe <rollsafe@users.noreply.github.com>
2016-06-27 20:03:13 -0400

    Implement ICodeListener to DataAnalyser

--- src/main/java/org/rsdeob/stdlib/ir/transform/DataAnalyser.java
+++ src/main/java/org/rsdeob/stdlib/ir/transform/DataAnalyser.java
@@ -3,12 +3,13 @@ package org.rsdeob.stdlib.ir.transform;
 import org.rsdeob.stdlib.cfg.edge.FlowEdge;
 import org.rsdeob.stdlib.collections.graph.FastGraphVertex;
 import org.rsdeob.stdlib.collections.graph.flow.FlowGraph;
+import org.rsdeob.stdlib.ir.api.ICodeListener;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-public abstract class DataAnalyser<N extends FastGraphVertex, E extends FlowEdge<N>, S> {
+public abstract class DataAnalyser<N extends FastGraphVertex, E extends FlowEdge<N>, S> implements ICodeListener<N> {
 	
 	protected final FlowGraph<N, E> graph;
 	protected final Map<N, S> in;
@@ -25,12 +26,7 @@ public abstract class DataAnalyser<N extends FastGraphVertex, E extends FlowEdge
 	
 	private void run() {
 		init();
-		processQueue();
-	}
-	
-	public void remove(N n) {
-		in.remove(n);
-		out.remove(n);
+		commit();
 	}
 	
 	public S in(N n) {
@@ -47,21 +43,6 @@ public abstract class DataAnalyser<N extends FastGraphVertex, E extends FlowEdge
 
 	public abstract void enqueue(N n);
 	
-	protected abstract void updateImpl(N n);
-	
-	public void update(N n) {
-		queue.add(n);
-		updateImpl(n);
-	}
-	
-	protected abstract void replaceImpl(N old, N n);
-	
-	public void replace(N old, N n) {
-		queue.add(n);
-		replaceImpl(old, n);
-		remove(old);
-	}
-	
 	protected void init() {
 		// set initial data states
 		for(N n : graph.vertices()) {
@@ -69,8 +50,6 @@ public abstract class DataAnalyser<N extends FastGraphVertex, E extends FlowEdge
 			out.put(n, newState());
 		}
 	}
-	
-	protected abstract void processQueue();
 	
 	protected abstract S newState();
 
@@ -89,4 +68,27 @@ public abstract class DataAnalyser<N extends FastGraphVertex, E extends FlowEdge
 	}
 	
 	protected abstract void apply(N n, S currentOut, S currentIn);
+
+	// ICodeListener overrides
+	@Override
+	public void updated(N n) {
+		queue.add(n);
+	}
+
+	@Override
+	public void replaced(N old, N n) {
+		removed(old);
+		added(n);
+	}
+
+	@Override
+	public void added(N n) {
+		queue.add(n);
+	}
+
+	@Override
+	public void removed(N n) {
+		in.remove(n);
+		out.remove(n);
+	}
 }
