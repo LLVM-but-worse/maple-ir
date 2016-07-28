455988ef65187c38cac9ea28714aea4bb03ddb5a
ecx86 <ecx86@users.noreply.github.com>
2016-07-28 13:05:57 -0400

    Remove setGraph, pass graph as a parameter instead

--- src/main/java/org/mapleir/stdlib/collections/graph/dot/impl/CFGStatementDecorator.java
+++ src/main/java/org/mapleir/stdlib/collections/graph/dot/impl/CFGStatementDecorator.java
@@ -1,17 +1,20 @@
 package org.mapleir.stdlib.collections.graph.dot.impl;
 
 import org.mapleir.stdlib.cfg.BasicBlock;
+import org.mapleir.stdlib.cfg.ControlFlowGraph;
 import org.mapleir.stdlib.cfg.edge.FlowEdge;
 import org.mapleir.stdlib.ir.stat.Statement;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class CFGStatementDecorator extends ControlFlowGraphDecorator {
 	@Override
-	public void decorateNodeProperties(BasicBlock n, Map<String, Object> nprops) {
-		super.decorateNodeProperties(n, nprops);
-
+	public void decorateNodeProperties(ControlFlowGraph g, BasicBlock n, Map<String, Object> nprops) {
+		super.decorateNodeProperties(g, n, nprops);
+		List<BasicBlock> order = new ArrayList<>(g.vertices());
+		
 		StringBuilder sb = new StringBuilder();
 		sb.append(order.indexOf(n)).append(n.getId()).append(". ");
 		if((flags & OPT_DEEP) != 0) {
@@ -29,7 +32,7 @@ public class CFGStatementDecorator extends ControlFlowGraphDecorator {
 	}
 
 	@Override
-	public void decorateEdgeProperties(BasicBlock n, FlowEdge<BasicBlock> e, Map<String, Object> eprops) {
+	public void decorateEdgeProperties(ControlFlowGraph g, BasicBlock n, FlowEdge<BasicBlock> e, Map<String, Object> eprops) {
 		if((flags & OPT_DEEP) != 0)
 			eprops.put("label", e.getClass().getSimpleName().replace("Edge", ""));
 	}
