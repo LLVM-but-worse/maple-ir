82f395ebd083d3418e6b35df653d2d828464ad1e
Bibl_ <GenerallyCool@hotmail.com>
2016-08-03 02:16:50 +0100

    Rewrote LivenessDecorator to comply with new BlockCommentDecorator.

--- src/main/java/org/mapleir/stdlib/collections/graph/dot/impl/LivenessDecorator.java
+++ src/main/java/org/mapleir/stdlib/collections/graph/dot/impl/LivenessDecorator.java
@@ -1,53 +1,33 @@
 package org.mapleir.stdlib.collections.graph.dot.impl;
 
-import org.mapleir.stdlib.cfg.BasicBlock;
-import org.mapleir.stdlib.cfg.ControlFlowGraph;
-import org.mapleir.stdlib.cfg.edge.FlowEdge;
-import org.mapleir.stdlib.collections.graph.flow.FlowGraph;
-import org.mapleir.stdlib.ir.locals.Local;
-import org.mapleir.stdlib.ir.transform.ssa.SSABlockLivenessAnalyser;
-import org.mapleir.stdlib.ir.transform.ssa.SSALivenessAnalyser;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import org.mapleir.stdlib.collections.graph.FastGraph;
+import org.mapleir.stdlib.collections.graph.FastGraphEdge;
+import org.mapleir.stdlib.collections.graph.FastGraphVertex;
+import org.mapleir.stdlib.ir.transform.Liveness;
 
-public class LivenessDecorator extends BlockCommentDecorator<ControlFlowGraph, BasicBlock, FlowEdge<BasicBlock>> {
-	private SSABlockLivenessAnalyser blockLiveness;
-	private SSALivenessAnalyser liveness;
+public class LivenessDecorator<G extends FastGraph<N, E>, N extends FastGraphVertex, E extends FastGraphEdge<N>> extends BlockCommentDecorator<G, N, E> {
 	
-	public LivenessDecorator setBlockLiveness(SSABlockLivenessAnalyser blockLiveness) {
-		this.blockLiveness = blockLiveness;
-		applyComments();
-		return this;
-	}
+	private Liveness<N> liveness;
 	
-	public LivenessDecorator setLiveness(SSALivenessAnalyser liveness) {
+	public LivenessDecorator<G, N, E> setLiveness(Liveness<N> liveness) {
 		this.liveness = liveness;
-		applyComments();
 		return this;
 	}
 	
-	private LivenessDecorator applyComments() {
-		clearComments();
-		FlowGraph<BasicBlock, FlowEdge<BasicBlock>> graph = blockLiveness != null ? blockLiveness.getGraph() : liveness.getGraph();
-		for (BasicBlock n : graph.vertices()) {
-			if (blockLiveness != null) {
-				addStartComment(n, "IN: " + blockLiveness.in(n).toString());
-				addEndComment(n, "OUT: " + blockLiveness.out(n).toString());
-			} else if (liveness != null) {
-				Set<Local> liveIn = new HashSet<>();
-				for (Map.Entry<Local, Boolean> e : liveness.in(n).entrySet())
-					if (e.getValue())
-						liveIn.add(e.getKey());
-				Set<Local> liveOut = new HashSet<>();
-				for (Map.Entry<Local, Boolean> e : liveness.out(n).entrySet())
-					if (e.getValue())
-						liveOut.add(e.getKey());
-				addStartComment(n, "IN: " + liveIn.toString());
-				addEndComment(n, "OUT: " + liveOut.toString());
-			}
-		}
-		return this;
+	@Override
+	public List<String> getVertexStartComments(G g, N n) {
+		List<String> list = new ArrayList<>();
+		list.add("IN: " + liveness.in(n));
+		return list;
+	}
+
+	@Override
+	public List<String> getVertexEndComments(G g, N n) {
+		List<String> list = new ArrayList<>();
+		list.add("OUT: " + liveness.out(n));
+		return list;
 	}
 }
