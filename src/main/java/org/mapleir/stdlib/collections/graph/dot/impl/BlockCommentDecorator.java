927e06f30e054c174e40b0fbe5586ad1cb6ff9fb
Bibl_ <GenerallyCool@hotmail.com>
2016-08-03 02:16:26 +0100

    Rewrote BlockCommentDecorator.
    Made BlockCommentDecorator do stateless on-demand commenting rather than
    stateful caching.

--- src/main/java/org/mapleir/stdlib/collections/graph/dot/impl/BlockCommentDecorator.java
+++ src/main/java/org/mapleir/stdlib/collections/graph/dot/impl/BlockCommentDecorator.java
@@ -1,50 +1,41 @@
 package org.mapleir.stdlib.collections.graph.dot.impl;
 
-import org.mapleir.stdlib.collections.ListCreator;
-import org.mapleir.stdlib.collections.NullPermeableHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.mapleir.stdlib.collections.graph.FastGraph;
 import org.mapleir.stdlib.collections.graph.FastGraphEdge;
 import org.mapleir.stdlib.collections.graph.FastGraphVertex;
 import org.mapleir.stdlib.collections.graph.dot.DotPropertyDecorator;
 
-import java.util.List;
-import java.util.Map;
-
-public class BlockCommentDecorator<G extends FastGraph<N, E>, N extends FastGraphVertex, E extends FastGraphEdge<N>> implements DotPropertyDecorator<G, N, E> {
-	private final NullPermeableHashMap<N, List<String>> vertexStartComments;
-	private final NullPermeableHashMap<N, List<String>> vertexEndComments;
+public abstract class BlockCommentDecorator<G extends FastGraph<N, E>, N extends FastGraphVertex, E extends FastGraphEdge<N>> implements DotPropertyDecorator<G, N, E> {
 	
-	public BlockCommentDecorator() {
-		vertexStartComments = new NullPermeableHashMap<>(new ListCreator<>());
-		vertexEndComments = new NullPermeableHashMap<>(new ListCreator<>());
-	}
+	public abstract List<String> getVertexStartComments(G g, N n);
 	
-	public BlockCommentDecorator<G, N, E> addStartComment(N n, String comment) {
-		vertexStartComments.getNonNull(n).add(comment);
-		return this;
-	}
-	
-	public BlockCommentDecorator<G, N, E> addEndComment(N n, String comment) {
-		vertexEndComments.getNonNull(n).add(comment);
-		return this;
-	}
-	
-	public BlockCommentDecorator<G, N, E> clearComments() {
-		vertexStartComments.clear();
-		vertexEndComments.clear();
-		return this;
-	}
+	public abstract List<String> getVertexEndComments(G g, N n);
 	
 	@Override
 	public void decorateNodeProperties(G g, N n, Map<String, Object> nprops) {
 		if (nprops.containsKey("label")) {
 			StringBuilder sb = new StringBuilder();
-			for (String comment : vertexStartComments.getNonNull(n))
-				sb.append("// ").append(comment).append("\\l");
+			
+			List<String> startComments = getVertexStartComments(g, n);
+			if(startComments != null) {
+				for (String comment : startComments) {
+					sb.append("// ").append(comment).append("\\l");
+				}
+			}
+			
 			sb.append(nprops.get("label").toString());
 			sb.append("\\l");
-			for (String comment : vertexEndComments.getNonNull(n))
-				sb.append("// ").append(comment).append("\\l");
+			
+			List<String> endComments = getVertexEndComments(g, n);
+			if(endComments != null) {
+				for (String comment : endComments) {
+					sb.append("// ").append(comment).append("\\l");
+				}
+			}
+			
 			nprops.put("label", sb.toString());
 		}
 	}
