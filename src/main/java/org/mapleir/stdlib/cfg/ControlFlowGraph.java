03375ac618cddbc4b417ae2cc3879853012c7239
Bibl_ <GenerallyCool@hotmail.com>
2016-08-07 22:32:43 +0100

    Added copy to ControlFlowGraph.

--- src/main/java/org/mapleir/stdlib/cfg/ControlFlowGraph.java
+++ src/main/java/org/mapleir/stdlib/cfg/ControlFlowGraph.java
@@ -1,5 +1,10 @@
 package org.mapleir.stdlib.cfg;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.mapleir.stdlib.collections.graph.util.GraphUtils;
 import org.objectweb.asm.tree.MethodNode;
 
@@ -7,7 +12,7 @@ public class ControlFlowGraph extends FastBlockGraph {
 	
 	private final MethodNode method;
 	
-	ControlFlowGraph(MethodNode method) {
+	public ControlFlowGraph(MethodNode method) {
 		this.method = method;
 	}
 	
@@ -18,5 +23,22 @@ public class ControlFlowGraph extends FastBlockGraph {
 	@Override
 	public String toString() {
 		return GraphUtils.toString(this, vertices());
+	}
+	
+	public ControlFlowGraph copy() {
+		ControlFlowGraph cfg = new ControlFlowGraph(method);
+		copy(map, cfg.map);
+		copy(reverseMap, cfg.reverseMap);
+		cfg.blockLabels.putAll(blockLabels);
+		cfg.ranges.addAll(ranges);
+		cfg.vertexIds.putAll(vertexIds);
+		cfg.entries.addAll(entries);
+		return cfg;
+	}
+	
+	private static <K, V> void copy(Map<K, Set<V>> src, Map<K, Set<V>> dst) {
+		for(Entry<K, Set<V>> e : src.entrySet()) {
+			dst.put(e.getKey(), new HashSet<>(e.getValue()));
+		}
 	}
 }
