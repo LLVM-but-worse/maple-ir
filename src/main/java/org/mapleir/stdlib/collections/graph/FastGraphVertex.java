178db95d72a647c8d9d8befd6fbc78edf1a38068
rcx <rcx@users.noreply.github.com>
2016-08-07 23:55:34 -0400

    Tv computer

--- src/main/java/org/mapleir/stdlib/collections/graph/FastGraphVertex.java
+++ src/main/java/org/mapleir/stdlib/collections/graph/FastGraphVertex.java
@@ -1,6 +1,12 @@
 package org.mapleir.stdlib.collections.graph;
 
+import org.mapleir.stdlib.cfg.util.LabelHelper;
+
 public interface FastGraphVertex {
 
 	String getId();
+	
+	default int getIndex() {
+		return LabelHelper.numeric(getId());
+	}
 }
