fcaabaadae106289e9fe590f46618b2f17433452
rcx <rcx@users.noreply.github.com>
2016-08-08 11:05:43 -0400

    temp

--- src/main/java/org/mapleir/stdlib/collections/graph/FastGraphVertex.java
+++ src/main/java/org/mapleir/stdlib/collections/graph/FastGraphVertex.java
@@ -1,11 +1,13 @@
 package org.mapleir.stdlib.collections.graph;
 
 import org.mapleir.stdlib.cfg.util.LabelHelper;
+import org.mapleir.stdlib.collections.BitSetElement;
 
-public interface FastGraphVertex {
+public interface FastGraphVertex extends BitSetElement {
 
 	String getId();
 	
+	@Override
 	default int getIndex() {
 		return LabelHelper.numeric(getId());
 	}
