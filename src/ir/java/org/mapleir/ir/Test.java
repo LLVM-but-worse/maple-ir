508953398abf2121472b0ba84260bd2111da5e98
Bibl_ <GenerallyCool@hotmail.com>
2016-08-16 15:43:22 +0100

    rename ssa bug.

--- src/ir/java/org/mapleir/ir/Test.java
+++ src/ir/java/org/mapleir/ir/Test.java
@@ -1,10 +1,12 @@
 package org.mapleir.ir;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import org.mapleir.AnalyticsTest;
 import org.mapleir.ir.cfg.ControlFlowGraph;
 import org.mapleir.ir.cfg.ControlFlowGraphBuilder;
 import org.objectweb.asm.ClassReader;
@@ -14,31 +16,26 @@ import org.objectweb.asm.tree.MethodNode;
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		ClassReader cr = new ClassReader(AnalyticsTest.class.getCanonicalName());
-		ClassNode cn = new ClassNode();
-		cr.accept(cn, 0);
-		
-//		InputStream i = new FileInputStream(new File("res/a.class"));
-//		ClassReader cr = new ClassReader(i);
+//		ClassReader cr = new ClassReader(AnalyticsTest.class.getCanonicalName());
 //		ClassNode cn = new ClassNode();
 //		cr.accept(cn, 0);
+		
+		InputStream i = new FileInputStream(new File("res/a.class"));
+		ClassReader cr = new ClassReader(i);
+		ClassNode cn = new ClassNode();
+		cr.accept(cn, 0);
 		
 		Iterator<MethodNode> it = new ArrayList<>(cn.methods).listIterator();
 		while(it.hasNext()) {
 			MethodNode m = it.next();
 
-			if(!m.toString().equals("org/mapleir/AnalyticsTest.test111()V")) {
+			if(!m.toString().equals("a/a/f/a.<init>()V")) {
 				continue;
 			}
 			
 			System.out.println("Processing " + m + "\n");
-			long now = System.nanoTime();
-			for(int i1=0; i1 < 5009; i1++) {
-				ControlFlowGraph cfg = ControlFlowGraphBuilder.build(m);
-			}
-			long el = System.nanoTime() - now;
-			
-			System.out.println("el: " + ((double)el/1000000) + "ms");
+			ControlFlowGraph cfg = ControlFlowGraphBuilder.build(m);
+			System.out.println(cfg);
 			
 //			SSABlockLivenessAnalyser live = new SSABlockLivenessAnalyser(cfg);
 //			live.compute();
