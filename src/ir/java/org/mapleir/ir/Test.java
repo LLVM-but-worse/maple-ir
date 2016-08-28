c302c3fd63ed068284b7f69b9def068587c226cc
Bibl_ <GenerallyCool@hotmail.com>
2016-08-28 02:55:53 +0100

    dump.

--- src/ir/java/org/mapleir/ir/Test.java
+++ src/ir/java/org/mapleir/ir/Test.java
@@ -2,21 +2,31 @@ package org.mapleir.ir;
 
 import static org.mapleir.ir.dot.ControlFlowGraphDecorator.OPT_DEEP;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import org.mapleir.ir.cfg.BasicBlock;
 import org.mapleir.ir.cfg.BoissinotDestructor;
 import org.mapleir.ir.cfg.ControlFlowGraph;
 import org.mapleir.ir.cfg.builder.ControlFlowGraphBuilder;
+import org.mapleir.ir.code.stmt.Statement;
 import org.mapleir.ir.dot.ControlFlowGraphDecorator;
+import org.mapleir.stdlib.cfg.edge.DummyEdge;
 import org.mapleir.stdlib.cfg.edge.FlowEdge;
 import org.mapleir.stdlib.collections.graph.dot.BasicDotConfiguration;
 import org.mapleir.stdlib.collections.graph.dot.DotConfiguration;
 import org.mapleir.stdlib.collections.graph.dot.DotWriter;
+import org.mapleir.stdlib.collections.graph.flow.ExceptionRange;
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public class Test {
@@ -145,6 +155,58 @@ public class Test {
 		return 114;
 	}
 	
+	public static void dump(ControlFlowGraph cfg, MethodNode m) {
+		m.visitCode();
+		m.instructions.clear();
+		m.tryCatchBlocks.clear();
+		
+		for(BasicBlock b : cfg.vertices()) {
+			b.resetLabel();
+		}
+		
+		for(BasicBlock b : cfg.vertices()) {
+			m.visitLabel(b.getLabel());
+			for(Statement stmt : b) {
+				stmt.toCode(m, null);
+			}
+		}
+
+		for(ExceptionRange<BasicBlock> er : cfg.getRanges()) {
+			String type = null;
+			Set<String> typeSet = er.getTypes();
+			if(typeSet.size() == 0 || typeSet.size() > 1) {
+				// TODO: fix base exception
+				type = Throwable.class.getCanonicalName().replace(".", "/");
+			} else {
+				// size == 1
+				type = typeSet.iterator().next();
+			}
+			List<BasicBlock> range = er.get();
+			Label start = range.get(0).getLabel();
+			Label end = null;
+			BasicBlock endBlock = range.get(range.size() - 1);
+			BasicBlock im = endBlock.getImmediate();
+			if (im == null) {
+				BasicBlock nextBlock = cfg.getBlock(BasicBlock.createBlockName(BasicBlock.numeric(endBlock.getId()) + 1));
+				if (nextBlock != null) {
+					end = nextBlock.getLabel();
+				} else {
+					LabelNode label = new LabelNode();
+					m.visitLabel(label.getLabel());
+					BasicBlock newExit = new BasicBlock(cfg, endBlock.getNumericId() + 1, label);
+					cfg.addVertex(newExit);
+					cfg.addEdge(endBlock, new DummyEdge<>(endBlock, newExit));
+					end = label.getLabel();
+				}
+			} else {
+				end = im.getLabel();
+			}
+			Label handler = er.getHandler().getLabel();
+			m.visitTryCatchBlock(start, end, handler, type);
+		}
+		m.visitEnd();
+	}
+	
 	public static void main(String[] args) throws IOException {
 		ClassReader cr = new ClassReader(Test.class.getCanonicalName());
 		ClassNode cn = new ClassNode();
@@ -163,7 +225,7 @@ public class Test {
 //				continue;
 //			}
 			
-			if(!m.toString().equals("org/mapleir/ir/Test.test115()V")) {
+			if(!m.toString().equals("org/mapleir/ir/Test.test118()V")) {
 				continue;
 			}
 			
@@ -179,6 +241,8 @@ public class Test {
 			} catch(RuntimeException e) {
 				throw new RuntimeException("\n" + cfg.toString(), e);
 			}
+			
+			cfg.getLocals().realloc(cfg);
 
 			BasicDotConfiguration<ControlFlowGraph, BasicBlock, FlowEdge<BasicBlock>> config = new BasicDotConfiguration<>(DotConfiguration.GraphType.DIRECTED);
 			DotWriter<ControlFlowGraph, BasicBlock, FlowEdge<BasicBlock>> writer = new DotWriter<>(config, cfg);
@@ -190,6 +254,18 @@ public class Test {
 //			System.out.println();
 //			System.out.println();
 			System.out.println(cfg);
+			
+			MethodNode m2 = new MethodNode(m.owner, m.access, m.name, m.desc, m.signature, m.exceptions.toArray(new String[0]));
+			dump(cfg, m2);
+			cn.methods.add(m2);
+			cn.methods.remove(m);
 		}
+		
+		ClassWriter clazz = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+		cn.accept(clazz);
+		byte[] saved = clazz.toByteArray();
+		FileOutputStream out = new FileOutputStream(new File("out/testclass.class"));
+		out.write(saved, 0, saved.length);
+		out.close();
 	}
 }
