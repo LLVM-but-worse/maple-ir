70b3192608c9802a065f298fc1526c40fc194b6f
rollsafe <rollsafe@users.noreply.github.com>
2016-09-02 23:10:12 -0400

    Fix compile errors

--- src/main/java/org/mapleir/deobimpl/ControlFlowFixerPhase.java
+++ src/main/java/org/mapleir/deobimpl/ControlFlowFixerPhase.java
@@ -1,44 +1,44 @@
-package org.mapleir.deobimpl;
-
-import java.util.List;
-
-import org.mapleir.ir.cfg.BasicBlock;
-import org.mapleir.ir.cfg.ControlFlowGraph;
-import org.mapleir.ir.cfg.builder.ControlFlowGraphBuilder;
-import org.mapleir.stdlib.IContext;
-import org.mapleir.stdlib.cfg.util.ControlFlowGraphDeobfuscator;
-import org.mapleir.stdlib.collections.graph.util.GraphUtils;
-import org.mapleir.stdlib.deob.IPhase;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
-
-public class ControlFlowFixerPhase implements IPhase {
-	public static final String KEY_ID = ControlFlowFixerPhase.class.getCanonicalName();
-	
-	private ControlFlowGraphDeobfuscator impl = new ControlFlowGraphDeobfuscator();
-	
-	@Override
-	public String getId() {
-		return KEY_ID;
-	}
-
-	@Override
-	public void accept(IContext cxt, IPhase prev, List<IPhase> completed) {
-		for(ClassNode cn : cxt.getNodes().values()) {
-			for(MethodNode m : cn.methods) {
-				if(m.instructions.size() > 0) {
-					ControlFlowGraph cfg = ControlFlowGraphBuilder.create(m);
-					List<BasicBlock> reordered = impl.deobfuscate(cfg);
-					impl.removeEmptyBlocks(cfg, reordered);
-					m.instructions = GraphUtils.recreate(cfg, reordered, true);
-				}
-			}
-		}
-		
-
-		
-		System.out.printf("   Pruned %d gotos.%n", impl.prunedGotos);
-		System.out.printf("   Removed %d empty blocks.%n", impl.removedEmpty);
-		System.out.printf("   Inlined %d strongly connected components.%n", impl.inlinedComponents);
-	}
-}
+//package org.mapleir.deobimpl;
+//
+//import java.util.List;
+//
+//import org.mapleir.ir.cfg.BasicBlock;
+//import org.mapleir.ir.cfg.ControlFlowGraph;
+//import org.mapleir.ir.cfg.builder.ControlFlowGraphBuilder;
+//import org.mapleir.stdlib.IContext;
+//import org.mapleir.stdlib.cfg.util.ControlFlowGraphDeobfuscator;
+//import org.mapleir.stdlib.collections.graph.util.GraphUtils;
+//import org.mapleir.stdlib.deob.IPhase;
+//import org.objectweb.asm.tree.ClassNode;
+//import org.objectweb.asm.tree.MethodNode;
+//
+//public class ControlFlowFixerPhase implements IPhase {
+//	public static final String KEY_ID = ControlFlowFixerPhase.class.getCanonicalName();
+//
+//	private ControlFlowGraphDeobfuscator impl = new ControlFlowGraphDeobfuscator();
+//
+//	@Override
+//	public String getId() {
+//		return KEY_ID;
+//	}
+//
+//	@Override
+//	public void accept(IContext cxt, IPhase prev, List<IPhase> completed) {
+//		for(ClassNode cn : cxt.getNodes().values()) {
+//			for(MethodNode m : cn.methods) {
+//				if(m.instructions.size() > 0) {
+//					ControlFlowGraph cfg = ControlFlowGraphBuilder.create(m);
+//					List<BasicBlock> reordered = impl.deobfuscate(cfg);
+//					impl.removeEmptyBlocks(cfg, reordered);
+//					m.instructions = GraphUtils.recreate(cfg, reordered, true);
+//				}
+//			}
+//		}
+//
+//
+//
+//		System.out.printf("   Pruned %d gotos.%n", impl.prunedGotos);
+//		System.out.printf("   Removed %d empty blocks.%n", impl.removedEmpty);
+//		System.out.printf("   Inlined %d strongly connected components.%n", impl.inlinedComponents);
+//	}
+//}
--- src/main/java/org/mapleir/stdlib/ir/transform/impl/CodeAnalytics.java
+++ src/main/java/org/mapleir/stdlib/ir/transform/impl/CodeAnalytics.java
@@ -1,6 +1,6 @@
 package org.mapleir.stdlib.ir.transform.impl;
 
-import org.mapleir.ir.analysis.StatementGraph;
+//import org.mapleir.ir.analysis.StatementGraph;
 import org.mapleir.ir.cfg.ControlFlowGraph;
 import org.mapleir.stdlib.ir.CodeBody;
 
@@ -8,12 +8,12 @@ public class CodeAnalytics {
 
 	public final CodeBody code;
 	public final ControlFlowGraph cfg;
-	public final StatementGraph sgraph;
-	public final LivenessAnalyser liveness;
-	public final DefinitionAnalyser definitions;
-	
-	public CodeAnalytics(CodeBody code, ControlFlowGraph cfg, StatementGraph sgraph, LivenessAnalyser liveness,
-			DefinitionAnalyser definitions) {
+	public final Object sgraph;
+	public final Object liveness;
+	public final Object definitions;
+
+	public CodeAnalytics(CodeBody code, ControlFlowGraph cfg, Object sgraph, Object liveness,
+			Object definitions) {
 		this.code = code;
 		this.cfg = cfg;
 		this.sgraph = sgraph;
