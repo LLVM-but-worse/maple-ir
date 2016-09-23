92fd5690d091b644da450652be223ed7f887af85
matthew <matty@mattysmith.co.uk>
2016-09-23 18:58:57 +0100

    to use markdown is to be illegal

--- src/ir/java/org/mapleir/ir/DVBTest.java
+++ src/ir/java/org/mapleir/ir/DVBTest.java
@@ -13,42 +13,55 @@ import org.mapleir.ir.cfg.builder.ControlFlowGraphBuilder;
 import org.mapleir.ir.cfg.builder.SSAGenPass;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.commons.JSRInlinerAdapter;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public class DVBTest {
-	public static void main(String[] args) throws Exception {
-		for (int i = 1; i <= 23; i++) {
+	public static void main(String[] args) {
+		boolean[] statuses = new boolean[23];
+		for (int i = 2; i <= 23; i++) {
 			System.out.println(i);
-			InputStream is = new FileInputStream(new File(String.format("res/dvb/DVB%04d.class", i)));
-			ClassReader cr = new ClassReader(is);
-			ClassNode cn = new ClassNode();
-			cr.accept(cn, 0);
+			try {
+				InputStream is = new FileInputStream(new File(String.format("res/dvb/DVB%04d.class", i)));
+				ClassReader cr = new ClassReader(is);
+				ClassNode cn = new ClassNode();
+				cr.accept(cn, 0);
 
-			Iterator<MethodNode> it = new ArrayList<>(cn.methods).listIterator();
-			while (it.hasNext()) {
-				MethodNode m = it.next();
-				SSAGenPass.DO_SPLIT = true;
-				SSAGenPass.ULTRANAIVE = false;
-				SSAGenPass.SKIP_SIMPLE_COPY_SPLIT = true;
-				SSAGenPass.PRUNE_EDGES = true;
+				Iterator<MethodNode> it = new ArrayList<>(cn.methods).listIterator();
+				while (it.hasNext()) {
+					MethodNode m = it.next();
+					SSAGenPass.DO_SPLIT = true;
+					SSAGenPass.ULTRANAIVE = false;
+					SSAGenPass.SKIP_SIMPLE_COPY_SPLIT = true;
+					SSAGenPass.PRUNE_EDGES = true;
 
-				ControlFlowGraph cfg = ControlFlowGraphBuilder.build(m);
-				new BoissinotDestructor(cfg, 0);
-				cfg.getLocals().realloc(cfg);
-				MethodNode m2 = new MethodNode(m.owner, m.access, m.name, m.desc, m.signature,
-						m.exceptions.toArray(new String[0]));
-				ControlFlowGraphDumper.dump(cfg, m2);
-				cn.methods.add(m2);
-				cn.methods.remove(m);
+					ControlFlowGraph cfg = ControlFlowGraphBuilder.build(m);
+					new BoissinotDestructor(cfg, 0);
+					cfg.getLocals().realloc(cfg);
+					MethodNode m2 = new MethodNode(m.owner, m.access, m.name, m.desc, m.signature,
+							m.exceptions.toArray(new String[0]));
+					ControlFlowGraphDumper.dump(cfg, m2);
+					cn.methods.add(m2);
+					cn.methods.remove(m);
+				}
+				ClassWriter clazz = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+				cn.accept(clazz);
+				byte[] saved = clazz.toByteArray();
+				FileOutputStream out = new FileOutputStream(
+						new File(String.format("res/dvb/processed_DVB%04d.class", i)));
+				out.write(saved, 0, saved.length);
+				out.close();
+				statuses[i - 1] = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				// System.exit(-1);
+				statuses[i - 1] = false;
 			}
-			ClassWriter clazz = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-			cn.accept(clazz);
-			byte[] saved = clazz.toByteArray();
-			FileOutputStream out = new FileOutputStream(
-					new File(String.format("res/dvb/processed_DVB%04d.class", i)));
-			out.write(saved, 0, saved.length);
-			out.close();
 		}
+
+		System.out.println("for the wiki");
+		for (int i = 0; i < statuses.length; i++)
+			System.out.printf("|%04d|%s|n/a|\n", i + 1, statuses[i] ? "PASS" : "FAIL");
 	}
 }
