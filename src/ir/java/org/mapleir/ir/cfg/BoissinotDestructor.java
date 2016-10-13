2d160da0cbe20995b6f257689b1079fab97d8171
Bibl_ <GenerallyCool@hotmail.com>
2016-10-13 22:33:09 +0100

    x

--- src/ir/java/org/mapleir/ir/cfg/BoissinotDestructor.java
+++ src/ir/java/org/mapleir/ir/cfg/BoissinotDestructor.java
@@ -88,9 +88,6 @@ public class BoissinotDestructor {
 		cfg.getEntries().clear();
 		cfg.getEntries().add(head);
 		
-		System.out.println("modded");
-		System.out.println(cfg);
-		
 		resolver = new DominanceLivenessAnalyser(cfg, head, null);
 		
 		insertCopies();
@@ -305,8 +302,6 @@ public class BoissinotDestructor {
 	// ============================================ Value interference ============================================= //
 	// ============================================================================================================= //
 	private void computeValueInterference() {
-		System.out.println("Preorder: " + GraphUtils.toBlockArray(dom_dfs.preorder));
-		System.out.println("PostORder: " + GraphUtils.toBlockArray(dom_dfs.postorder));
 		for (int i = dom_dfs.postorder.size() - 1; i >= 0; i--) {
 			BasicBlock bl = dom_dfs.postorder.get(i);
 			for (Statement stmt : bl) {
@@ -330,7 +325,6 @@ public class BoissinotDestructor {
 					for (CopyPair p : copy.pairs) {
 						LinkedHashSet<Local> valueClass = values.getNonNull(p.source);
 						valueClass.add(p.targ);
-						System.out.println(p.source);
 						values.put(p.targ, valueClass);
 					}
 				}
