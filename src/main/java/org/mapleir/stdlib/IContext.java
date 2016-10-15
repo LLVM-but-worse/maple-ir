d9ad2f1ecbdcf3bc90c730c23defb371603ea6ac
Bibl_ <GenerallyCool@hotmail.com>
2016-10-15 23:54:26 +0100

    Removed getCfg from IContext (refers to old cfg impl).

--- src/main/java/org/mapleir/stdlib/IContext.java
+++ src/main/java/org/mapleir/stdlib/IContext.java
@@ -1,13 +1,9 @@
 package org.mapleir.stdlib;
 
-import org.mapleir.ir.cfg.ControlFlowGraph;
 import org.mapleir.stdlib.collections.NodeTable;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
 
 public interface IContext {
 
 	NodeTable<ClassNode> getNodes();
-	
-	ControlFlowGraph createControlFlowGraph(MethodNode m);
 }
