c10262b6f86c990c400081d96478b42797c0b946
Bibl_ <GenerallyCool@hotmail.com>
2016-10-15 22:33:45 +0100

    Split printer into blockToString method.

--- src/ir/java/org/mapleir/ir/cfg/ControlFlowGraph.java
+++ src/ir/java/org/mapleir/ir/cfg/ControlFlowGraph.java
@@ -40,49 +40,52 @@ public class ControlFlowGraph extends FastBlockGraph {
 		int insn = 0;
 		
 		for(BasicBlock b : vertices()) {
-			// sw.print("===#Block " + b.getId() + "(size=" + (b.size()) + ")===");
-			sw.print("===#Block " + b.getId() + "(size=" + (b.size()) + ", ident=" + (b.getLabelNode() != null ? b.getLabelNode().hashCode() : "null") + ")===");
-			sw.tab();
+			blockToString(sw, this, b, insn);
+		}
+		return sw.toString();
+	}
+	
+	public static void blockToString(TabbedStringWriter sw, ControlFlowGraph cfg, BasicBlock b, int insn) {
+		// sw.print("===#Block " + b.getId() + "(size=" + (b.size()) + ")===");
+		sw.print("===#Block " + b.getId() + "(size=" + (b.size()) + ", ident=" + (b.getLabelNode() != null ? b.getLabelNode().hashCode() : "null") + ")===");
+		sw.tab();
+		
+		Iterator<Statement> it = b.iterator();
+		if(!it.hasNext()) {
+			sw.untab();
+		} else {
+			sw.print("\n");
+		}
+		while(it.hasNext()) {
+			Statement stmt = it.next();
+//			sw.print(stmt.getId() + ". ");
+			sw.print(insn++ + ". ");
+			stmt.toString(sw);
 			
-			Iterator<Statement> it = b.iterator();
 			if(!it.hasNext()) {
 				sw.untab();
 			} else {
 				sw.print("\n");
 			}
-			while(it.hasNext()) {
-				Statement stmt = it.next();
-//				sw.print(stmt.getId() + ". ");
-				sw.print(insn++ + ". ");
-				stmt.toString(sw);
-				
-				if(!it.hasNext()) {
-					sw.untab();
-				} else {
-					sw.print("\n");
-				}
-			}
-
-			sw.tab();
-			sw.tab();
-			
-			for(FlowEdge<BasicBlock> e : getEdges(b)) {
-				sw.print("\n-> " + e.toString());
-			}
-
-			for(FlowEdge<BasicBlock> p : getReverseEdges(b)) {
-				sw.print("\n<- " + p.toString());
-			}
-
-			sw.untab();
-			sw.untab();
-			
-			sw.print("\n");
-			
 		}
-		return sw.toString();
+
+		sw.tab();
+		sw.tab();
+		
+		for(FlowEdge<BasicBlock> e : cfg.getEdges(b)) {
+			sw.print("\n-> " + e.toString());
+		}
+
+		for(FlowEdge<BasicBlock> p : cfg.getReverseEdges(b)) {
+			sw.print("\n<- " + p.toString());
+		}
+
+		sw.untab();
+		sw.untab();
+		
+		sw.print("\n");
 	}
-	
+
 	@Override
 	public ControlFlowGraph copy() {
 		return new ControlFlowGraph(this);
