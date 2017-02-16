fe0a6f609fb40f3b3326df239ba7039a30afb27b
Bibl_ <GenerallyCool@hotmail.com>
2017-02-16 13:36:15 +0000

    clean

--- src/main/java/org/mapleir/deobimpl2/DeadCodeEliminationPass.java
+++ src/main/java/org/mapleir/deobimpl2/DeadCodeEliminationPass.java
@@ -1,62 +1,69 @@
 package org.mapleir.deobimpl2;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 
 import org.mapleir.ir.cfg.BasicBlock;
 import org.mapleir.ir.cfg.ControlFlowGraph;
 import org.mapleir.ir.cfg.edge.FlowEdge;
-import org.mapleir.ir.cfg.edge.FlowEdges;
-import org.mapleir.ir.cfg.edge.TryCatchEdge;
 import org.mapleir.ir.code.Stmt;
 import org.mapleir.ir.code.stmt.copy.CopyPhiStmt;
 import org.mapleir.stdlib.IContext;
 import org.mapleir.stdlib.deob.ICompilerPass;
-import org.mapleir.stdlib.util.TabbedStringWriter;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public class DeadCodeEliminationPass implements ICompilerPass {
 
+	public static void safeKill(FlowEdge<BasicBlock> fe) {
+		for (Stmt stmt : fe.dst) {
+			if (stmt.getOpcode() == Stmt.PHI_STORE) {
+				CopyPhiStmt phs = (CopyPhiStmt) stmt;
+				phs.getExpression().removeArgument(fe.src);
+			} else {
+				return;
+			}
+		}
+	}
+	
 	public static int process(ControlFlowGraph cfg) {
 		int i = 0;
+
+		boolean c;
 		
-		boolean changed;
 		do {
-			changed = false;
+			c = false;
 			for (BasicBlock b : new HashSet<>(cfg.vertices())) {
 				if (cfg.getReverseEdges(b).size() == 0 && !cfg.getEntries().contains(b)) {
-					for (FlowEdge<BasicBlock> fe : cfg.getEdges(b))
-						for (Stmt stmt : fe.dst)
-							if (stmt.getOpcode() == Stmt.PHI_STORE)
-								((CopyPhiStmt) stmt).getExpression().getSources().remove(b);
-							else break;
+					
+					for (FlowEdge<BasicBlock> fe : cfg.getEdges(b)) {
+						safeKill(fe);
+					}
 					cfg.removeVertex(b);
 					
 					i++;
-					changed = true;
+					c = true;
 				}
 			}
-		} while (changed);
-		
+		} while (c);
+
 		return i;
 	}
-	
+
 	@Override
 	public void accept(IContext cxt, ICompilerPass prev, List<ICompilerPass> completed) {
 		int i = 0;
-		
-		for(ClassNode cn : cxt.getClassTree().getClasses().values()) {
-			for(MethodNode m : cn.methods) {
+
+		for (ClassNode cn : cxt.getClassTree().getClasses().values()) {
+			for (MethodNode m : cn.methods) {
 				ControlFlowGraph cfg = cxt.getIR(m);
-				
+
 				/* dead blocks */
-				
+
 				i += process(cfg);
 			}
 		}
-		
+
 		System.out.printf("  removed %d dead blocks.%n", i);
 	}
 }
