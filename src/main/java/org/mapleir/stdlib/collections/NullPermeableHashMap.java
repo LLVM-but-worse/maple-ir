4e82ab561cff20715485688db9976df3c172c6df
rollsafe <rollsafe@users.noreply.github.com>
2017-02-19 21:00:57 -0500

    Fix constant uniqueness in ConstantParameterPass.

--- src/main/java/org/mapleir/stdlib/collections/NullPermeableHashMap.java
+++ src/main/java/org/mapleir/stdlib/collections/NullPermeableHashMap.java
@@ -6,15 +6,19 @@ public class NullPermeableHashMap<K, V> extends HashMap<K, V> {
 
 	private static final long serialVersionUID = 1L;
 
-	private final KeyedValueCreator<? super K, V> creator;
+	private final KeyedValueCreator<? super K, ? extends V> creator;
 
 	public NullPermeableHashMap(NullPermeableHashMap<K, V> map) {
 		super(map);
 		creator = map.creator;
 	}
 	
-	public NullPermeableHashMap(KeyedValueCreator<? super K, V> creator) {
+	public NullPermeableHashMap(KeyedValueCreator<? super K, ? extends V> creator) {
 		this.creator = creator;
+	}
+	
+	public NullPermeableHashMap(ValueCreator<? extends V> creator) {
+		this((KeyedValueCreator<? super K, ? extends V>) creator);
 	}
 
 	public NullPermeableHashMap() {
