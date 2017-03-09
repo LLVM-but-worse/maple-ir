cf81722679edb5b8009b70b19e56349fd87ee401
Bibl_ <GenerallyCool@hotmail.com>
2017-03-09 17:28:44 +0000

    Merged ClassHelper and ClassNodeUtil

--- src/main/java/org/mapleir/stdlib/klass/ClassHelper.java
+++ src/main/java/org/mapleir/stdlib/klass/ClassHelper.java
@@ -1,5 +1,7 @@
 package org.mapleir.stdlib.klass;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -8,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
 
 /**
@@ -42,5 +45,48 @@ public class ClassHelper {
 			list.add(it.next());
 		}
 		return list;
+	}
+	
+	public static ClassNode create(byte[] bytes) {
+		return create(bytes, ClassReader.SKIP_FRAMES);
+	}
+
+	public static ClassNode create(byte[] bytes, int flags) {
+		ClassReader reader = new ClassReader(bytes);
+		ClassNode node = new ClassNode();
+		reader.accept(node, flags);
+		return node;
+	}
+
+	public static ClassNode create(InputStream in) {
+		return create(in, ClassReader.SKIP_FRAMES);
+	}
+
+	public static ClassNode create(InputStream in, int flags) {
+		try {
+			ClassReader cr = new ClassReader(in);
+			ClassNode cs = new ClassNode();
+			cr.accept(cs, flags);
+			return cs;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static ClassNode create(String name) {
+		return create(name, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
+	}
+
+	public static ClassNode create(String name, int flags) {
+		try {
+			ClassReader cr = new ClassReader(name.replace(".", "/"));
+			ClassNode cs = new ClassNode();
+			cr.accept(cs, flags);
+			return cs;
+		} catch (IOException e) {
+			// TODO: log print
+			return null;
+		}
 	}
 }
