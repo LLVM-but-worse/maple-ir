0e87142017c8a9c97a8611eb0064eca13fd598e7
Bibl_ <GenerallyCool@hotmail.com>
2017-03-14 21:29:12 +0000

    :(

---
+++ src/ir/java/org/mapleir/ir/d.java
@@ -0,0 +1,24 @@
+package org.mapleir.ir;
+
+public abstract class d {
+
+	abstract d[][] lmao();
+	
+	static class x extends d {
+		@Override
+		x[][] lmao() {
+			return null;
+		}
+	}
+
+	static class y extends x {
+		@Override
+		y[][] lmao() {
+			return null;
+		}
+	}
+
+	void t(x x) {
+		x.lmao();
+	}
+}
