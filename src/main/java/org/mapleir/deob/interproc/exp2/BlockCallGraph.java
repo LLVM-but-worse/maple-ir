ccf22edbf7fee9ac70ca099889bb63660ea3c592
Bibl_ <GenerallyCool@hotmail.com>
2017-07-04 02:31:29 +0100

    Added current generation (wow) IPA classes (WIP)

---
+++ src/main/java/org/mapleir/deob/interproc/exp2/BlockCallGraph.java
@@ -0,0 +1,293 @@
+package org.mapleir.deob.interproc.exp2;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+import org.mapleir.ir.cfg.BasicBlock;
+import org.mapleir.ir.cfg.ControlFlowGraph;
+import org.mapleir.ir.cfg.edge.FlowEdge;
+import org.mapleir.ir.cfg.edge.FlowEdges;
+import org.mapleir.ir.cfg.edge.ImmediateEdge;
+import org.mapleir.ir.cfg.edge.TryCatchEdge;
+import org.mapleir.ir.code.Expr;
+import org.mapleir.ir.code.Opcode;
+import org.mapleir.ir.code.Stmt;
+import org.mapleir.ir.code.expr.PhiExpr;
+import org.mapleir.ir.code.stmt.ConditionalJumpStmt;
+import org.mapleir.ir.code.stmt.SwitchStmt;
+import org.mapleir.ir.code.stmt.UnconditionalJumpStmt;
+import org.mapleir.ir.code.stmt.copy.CopyPhiStmt;
+import org.mapleir.stdlib.collections.graph.FastDirectedGraph;
+import org.mapleir.stdlib.collections.graph.FastGraph;
+import org.mapleir.stdlib.collections.graph.flow.ExceptionRange;
+import org.objectweb.asm.tree.LabelNode;
+
+public class BlockCallGraph extends FastDirectedGraph<CallGraphBlock, FlowEdge<CallGraphBlock>> {
+
+	public static void prepareControlFlowGraph(ControlFlowGraph cfg) {
+		List<BasicBlock> order = new ArrayList<>();
+		Map<BasicBlock, BasicBlock> remap = new HashMap<>();
+		
+		for(BasicBlock originalBlock : new TreeSet<>(cfg.vertices())) {
+			order.add(originalBlock);
+			
+			List<List<Stmt>> subBlockStmts = new ArrayList<>();
+			List<Stmt> currentSubBlock = new ArrayList<>();
+			
+			for(Stmt stmt : originalBlock) {
+				currentSubBlock.add(stmt);
+				
+				if(statementContainsAnyInvocation(stmt)) {
+					/* split block */
+					subBlockStmts.add(currentSubBlock);
+					
+					currentSubBlock = new ArrayList<>();
+				}
+			}
+			
+			if(!currentSubBlock.isEmpty()) {
+				subBlockStmts.add(currentSubBlock);
+			}
+
+			/* if we have no subBlocks, it means the
+			 * original block was empty.
+			 * if we have 1 subBlock, there was no
+			 * invoke in the block.
+			 * in both cases we don't have to do anything. */
+			
+			int blockIDCounter = cfg.size() + 2;
+			
+			if(subBlockStmts.size() > 1) {
+				/* just need to transfer edges. have
+				 * incoming edges into the first block
+				 * in the list and link them by a single
+				 * immediate. also need to clone exception
+				 * edges. */
+				
+				List<BasicBlock> subBlocks = new ArrayList<>();
+				
+				List<Stmt> firstList = subBlockStmts.get(0);
+				
+				BasicBlock firstBlock = new BasicBlock(cfg, blockIDCounter++, new LabelNode());
+				remap.put(originalBlock, firstBlock);
+				
+				/* copy flags in case of special block (such as entry). */
+				firstBlock.setFlags(originalBlock.getFlags());
+				originalBlock.removeAll(firstList);
+				firstBlock.addAll(firstList);
+				subBlocks.add(firstBlock);
+				
+				BasicBlock lastBlock = firstBlock;
+				
+				Iterator<List<Stmt>> subBlockIterator = subBlockStmts.iterator();
+				subBlockIterator.next(); // skip first (already done).
+				while(subBlockIterator.hasNext()) {
+					List<Stmt> subBlock = subBlockIterator.next();
+					
+					BasicBlock currentBlock = new BasicBlock(cfg, blockIDCounter++, new LabelNode());
+					originalBlock.removeAll(subBlock);
+					currentBlock.addAll(subBlock);
+					subBlocks.add(currentBlock);
+					
+					cfg.addEdge(lastBlock, new ImmediateEdge<>(lastBlock, currentBlock));
+					lastBlock = currentBlock;
+				}
+				
+				if(originalBlock.size() != 0) {
+					throw new RuntimeException(Integer.toString(originalBlock.size()));
+				}
+				
+				/* lastBlock contains the last block now. */
+				
+				/* list of ranges this block was a participant in.
+				 * we need to create between each block in the new
+				 * region to the handler of this range. */
+				Set<ExceptionRange<BasicBlock>> originalBlockRanges = new HashSet<>();
+				
+				/* clone predecessor edges, src stays the same but 
+				 * leads to the first block of the new region. 
+				 * if the original block was a handler, we need to
+				 * set the new first block as the handler as well
+				 * as cloning the edges. */
+				Iterator<FlowEdge<BasicBlock>> predEdgesIterator = new HashSet<>(cfg.getReverseEdges(originalBlock)).iterator();
+				while(predEdgesIterator.hasNext()) {
+					FlowEdge<BasicBlock> predEdge = predEdgesIterator.next();
+					BasicBlock src = predEdge.src;
+					
+					boolean isExceptionEdge = predEdge.getType() == FlowEdges.TRYCATCH;
+					
+					/* special case (HAARD): self looping
+					 * blocks. */
+					if(src == originalBlock) {
+						src = lastBlock;
+					}
+					
+					/* FIXED: we need to do this here because
+					 * TryCatchEdge.clone is very unintuitive.
+					 * it uses the erange handler as it's dst
+					 * node. if we don't set the handler of the 
+					 * erange first, the dst stays the same. */
+					if(isExceptionEdge) {
+						TryCatchEdge<BasicBlock> tce = (TryCatchEdge<BasicBlock>) predEdge;
+						ExceptionRange<BasicBlock> owningRange = tce.erange;
+						
+						owningRange.setHandler(firstBlock);
+					}
+					
+					FlowEdge<BasicBlock> clonedPredEdge = predEdge.clone(src, firstBlock);
+					cfg.addEdge(src, clonedPredEdge);
+					
+					if(!isExceptionEdge) {
+						/* correct the flow function in the predecessor.
+						 * (no flow function for exception edge) */
+						if(src.size() > 0) {
+							Stmt lastStmt = src.get(src.size() - 1);
+							int lastStmtOpcode = lastStmt.getOpcode();
+							
+							switch(lastStmtOpcode) {
+								case Opcode.COND_JUMP: {
+									ConditionalJumpStmt j = (ConditionalJumpStmt) lastStmt;
+									if(j.getTrueSuccessor() == originalBlock) {
+										j.setTrueSuccessor(firstBlock);
+									}
+									break;
+								}
+								case Opcode.UNCOND_JUMP: {
+									UnconditionalJumpStmt j = (UnconditionalJumpStmt) lastStmt;
+									if(j.getTarget() != originalBlock) {
+										throw new IllegalStateException();
+									} else {
+										j.setTarget(firstBlock);
+									}
+									break;
+								}
+								case Opcode.SWITCH_JUMP: {
+									SwitchStmt s = (SwitchStmt) lastStmt;
+									for (Entry<Integer, BasicBlock> en : s.getTargets().entrySet()) {
+										BasicBlock t = en.getValue();
+										if (t == originalBlock) {
+											en.setValue(firstBlock);
+										}
+									}
+									break;
+								}
+							}
+						}
+					}
+				}
+				
+				/* clone only non exceptional successor edges, dst
+				 * is the same but comes from the new last block
+				 * of the new region. */
+				Iterator<FlowEdge<BasicBlock>> succEdgesIterator = new HashSet<>(cfg.getEdges(originalBlock)).iterator();
+				while(succEdgesIterator.hasNext()) {
+					FlowEdge<BasicBlock> succEdge = succEdgesIterator.next();
+
+					BasicBlock dst = succEdge.dst;
+					
+					if(succEdge.getType() != FlowEdges.TRYCATCH) {
+						if(dst == firstBlock) {
+							throw new RuntimeException("??");
+						}
+						
+						/* if it's a self loop, we have already added
+						 * the self loop edge above when dealing with
+						 * predecessors. */
+						if(dst != originalBlock) {
+							FlowEdge<BasicBlock> clonedSuccEdge = succEdge.clone(lastBlock, dst);
+							cfg.addEdge(lastBlock, clonedSuccEdge);
+						}
+					} else {
+						TryCatchEdge<BasicBlock> tce = (TryCatchEdge<BasicBlock>) succEdge;
+						originalBlockRanges.add(tce.erange);
+					}
+				}
+				
+				/* create edges from each block in the region
+				 * to the handlers and add each of the new blocks
+				 * to the range. */
+				for(ExceptionRange<BasicBlock> er : originalBlockRanges) {
+					for(BasicBlock b : subBlocks) {
+						cfg.addEdge(b, new TryCatchEdge<>(b, er));
+					}
+					
+					/* remove the original block from each of the
+					 * ranges and add the new ones. */
+					er.addVertices(originalBlock, subBlocks);
+					er.removeVertex(originalBlock);
+				}
+				
+				/* finally remove the original block completely. */
+				cfg.removeVertex(originalBlock);
+				order.addAll(subBlocks);
+				order.remove(originalBlock);
+				
+			}
+		}
+		
+		/* fix phi operand sources. */
+		
+		for(BasicBlock basicBlock : cfg.vertices()) {
+			for(Stmt stmt : basicBlock) {
+				if(stmt.getOpcode() == Opcode.PHI_STORE) {
+					CopyPhiStmt copyPhiStmt = (CopyPhiStmt) stmt;
+					PhiExpr phi = copyPhiStmt.getExpression();
+					
+					Map<BasicBlock, Expr> phiArgumentMap = phi.getArguments();
+					
+					for(BasicBlock originalSource : new TreeSet<>(phiArgumentMap.keySet())) {
+						if(remap.containsKey(originalSource)) {
+							phiArgumentMap.put(remap.get(originalSource), phiArgumentMap.remove(originalSource));
+						}
+					}
+				} else {
+					break;
+				}
+			}
+		}
+		
+		cfg.naturalise(order);
+	}
+	
+	private static boolean statementContainsAnyInvocation(Stmt stmt) {
+		for(Expr e : stmt.enumerateOnlyChildren()) {
+			int opcode = e.getOpcode();
+			
+			if(opcode == Opcode.INIT_OBJ || opcode == Opcode.INVOKE || opcode == Opcode.DYNAMIC_INVOKE) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean excavate(CallGraphBlock n) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean jam(CallGraphBlock pred, CallGraphBlock succ, CallGraphBlock n) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public FlowEdge<CallGraphBlock> clone(FlowEdge<CallGraphBlock> edge, CallGraphBlock oldN, CallGraphBlock newN) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FlowEdge<CallGraphBlock> invert(FlowEdge<CallGraphBlock> edge) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FastGraph<CallGraphBlock, FlowEdge<CallGraphBlock>> copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
