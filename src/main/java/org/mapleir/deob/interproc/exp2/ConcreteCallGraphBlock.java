9684c841edd941ca20b4e8a363fa253c061d125b
Bibl_ <GenerallyCool@hotmail.com>
2017-07-04 02:31:29 +0100

    Added current generation (wow) IPA classes (WIP)

---
+++ src/main/java/org/mapleir/deob/interproc/exp2/ConcreteCallGraphBlock.java
@@ -0,0 +1,12 @@
+package org.mapleir.deob.interproc.exp2;
+
+import org.mapleir.ir.cfg.BasicBlock;
+
+public class ConcreteCallGraphBlock extends CallGraphBlock {
+	public final BasicBlock block;
+	
+	public ConcreteCallGraphBlock(BasicBlock block, int id) {
+		super(id);
+		this.block = block;
+	}
+}
