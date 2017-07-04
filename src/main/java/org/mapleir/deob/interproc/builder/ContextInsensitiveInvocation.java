23addd3bcb43e556bd6836f7fce8e7012cd5bcb6
Bibl_ <GenerallyCool@hotmail.com>
2017-07-04 02:19:10 +0100

    Removed old IPA classes.

--- src/main/java/org/mapleir/deob/interproc/builder/ContextInsensitiveInvocation.java
+++
@@ -1,9 +0,0 @@
-package org.mapleir.deob.interproc.builder;
-
-import org.mapleir.stdlib.collections.graph.FastGraphEdge;
-
-public class ContextInsensitiveInvocation<T extends CallGraphNode> extends FastGraphEdge<T> {
-	public ContextInsensitiveInvocation(T src, T dst) {
-		super(src, dst);
-	}
-}
