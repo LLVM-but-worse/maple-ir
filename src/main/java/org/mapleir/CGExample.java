8c4ee96600e11e78aff1bde572ce28b3ee0bfaa5
Bibl_ <GenerallyCool@hotmail.com>
2017-07-04 02:19:55 +0100

    Added a few call graph testing classes (for now).

--- src/main/java/org/mapleir/CGExample.java
+++ src/main/java/org/mapleir/CGExample.java
@@ -16,14 +16,33 @@ public class CGExample {
 		exit();
 	}
 	
+	void entry2() {
+		head2();
+	}
+	
+	void head2() {
+		body2();
+	}
+	
 	void body2() {
+		exit2();
+	}
+	
+	void exit2() {
+		if(b) {
+			head2();
+		}
 	}
 	
 	void exit() {
 		if(b) {
 			head1();
 		} else {
-			body2();
+			entry2();
 		}
+	}
+	
+	public static void main(String[] args) {
+		new CGExample().entry();
 	}
 }
