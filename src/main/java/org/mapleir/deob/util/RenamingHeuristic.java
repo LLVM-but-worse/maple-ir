d966ebd3599974622539b4e1ad5ee636de981dee
Bibl_ <GenerallyCool@hotmail.com>
2017-07-04 02:24:12 +0100

    Added default RenamingHeuristics in RenamingHeuristic.

--- src/main/java/org/mapleir/deob/util/RenamingHeuristic.java
+++ src/main/java/org/mapleir/deob/util/RenamingHeuristic.java
@@ -2,6 +2,8 @@ package org.mapleir.deob.util;
 
 public interface RenamingHeuristic {
 	boolean shouldRename(String name, int access);
-	
+
+	RenamingHeuristic RENAME_NONE = (name, access) -> false;
 	RenamingHeuristic RENAME_ALL = (name, access) -> true;
+	RenamingHeuristic ALLATORI = (name, access) -> name.toLowerCase().equals("iiiiiiiiii");
 }
