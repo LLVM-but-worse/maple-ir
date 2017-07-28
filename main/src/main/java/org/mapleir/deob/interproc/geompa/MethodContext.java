6ceedac75833623ec62c2c9b510aa10c0e9e4e3b
Bibl_ <GenerallyCool@hotmail.com>
2017-07-29 00:54:00 +0100

    Implement MapleMethodOrMethodContext

--- main/src/main/java/org/mapleir/deob/interproc/geompa/MethodContext.java
+++ main/src/main/java/org/mapleir/deob/interproc/geompa/MethodContext.java
@@ -1,42 +1,57 @@
+package org.mapleir.deob.interproc.geompa;
+
+import java.util.HashMap;
 import java.util.Map;
 
-public final class MethodContext implements MethodOrMethodContext
-{ 
-    private SootMethod method;
-    @Override
-	public SootMethod method() { return method; }
-    private Context context;
-    @Override
-	public Context context() { return context; }
-    private MethodContext( SootMethod method, Context context ) {
-        this.method = method;
-        this.context = context;
-    }
-    @Override
+public final class MethodContext implements MapleMethodOrMethodContext {
+	private static final Map<MethodContext, MethodContext> map = new HashMap<>();
+	
+	private MapleMethod method;
+	private Context context;
+
+	private MethodContext(MapleMethod method, Context context) {
+		this.method = method;
+		this.context = context;
+	}
+
+	@Override
+	public MapleMethod method() {
+		return method;
+	}
+	
+	@Override
+	public Context context() {
+		return context;
+	}
+
+	@Override
 	public int hashCode() {
-        return method.hashCode() + context.hashCode();
-    }
-    @Override
-	public boolean equals( Object o ) {
-        if( o instanceof MethodContext ) {
-            MethodContext other = (MethodContext) o;
-            return method.equals( other.method ) && context.equals( other.context );
-        }
-        return false;
-    }
-    public static MethodOrMethodContext v( SootMethod method, Context context ) {
-        if( context == null ) return method;
-        MethodContext probe = new MethodContext( method, context );
-        Map<MethodContext, MethodContext> map = G.v().MethodContext_map;
-        MethodContext ret = map.get( probe );
-        if( ret == null ) {
-            map.put( probe, probe );
-            return probe;
-        }
-        return ret;
-    }
-    @Override
+		return method.hashCode() + context.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof MethodContext) {
+			MethodContext other = (MethodContext) o;
+			return method.equals(other.method) && context.equals(other.context);
+		}
+		return false;
+	}
+
+	public static MapleMethodOrMethodContext v(MapleMethod method, Context context) {
+		if (context == null)
+			return method;
+		MethodContext probe = new MethodContext(method, context);
+		MethodContext ret = map.get(probe);
+		if (ret == null) {
+			map.put(probe, probe);
+			return probe;
+		}
+		return ret;
+	}
+
+	@Override
 	public String toString() {
-        return "Method "+method+" in context "+context;
-    }
+		return "Method " + method + " in context " + context;
+	}
 }
