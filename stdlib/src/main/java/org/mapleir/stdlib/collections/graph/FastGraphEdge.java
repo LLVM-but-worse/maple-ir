d89b8ceb476393ae1abf7fa90e2a6ec0ec5c4559
rcx <rcx@users.noreply.github.com>
2017-08-05 15:13:43 -0400

    Begin converting FastGraphEdge to interface

--- stdlib/src/main/java/org/mapleir/stdlib/collections/graph/FastGraphEdge.java
+++ stdlib/src/main/java/org/mapleir/stdlib/collections/graph/FastGraphEdge.java
@@ -1,45 +1,16 @@
 package org.mapleir.stdlib.collections.graph;
 
-public class FastGraphEdge<N extends FastGraphVertex> implements Comparable<FastGraphEdge<N>> {
+public interface FastGraphEdge<N extends FastGraphVertex> extends Comparable<FastGraphEdge<N>> {
+	N src();
 
-	public final N src;
-	public final N dst;
-	
-	public FastGraphEdge(N src, N dst) {
-		this.src = src;
-		this.dst = dst;
-	}
+	N dst();
 
 	@Override
-	public int compareTo(FastGraphEdge<N> o) {
+	default int compareTo(FastGraphEdge<N> o) {
 		if (this.equals(o))
 			return 0;
 		else
-			return (int) Math.signum(2 * Integer.compare(src.getNumericId(), o.src.getNumericId())
-					+ Integer.compare(dst.getNumericId(), o.dst.getNumericId()));
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (o == null)
-			return false;
-		if (this == o)
-			return true;
-		if (!(o instanceof FastGraphEdge))
-			return false;
-		FastGraphEdge v = (FastGraphEdge) o;
-
-		// assert id/numericId are consistent
-		assert ((v.src.getNumericId() == src.getNumericId()) == (v.src.getId().equals(src.getId())));
-		assert ((v.dst.getNumericId() == dst.getNumericId()) == (v.dst.getId().equals(dst.getId())));
-
-		return v.src.getNumericId() == src.getNumericId() && v.dst.getNumericId() == dst.getNumericId();
-	}
-
-	@Override
-	public int hashCode() {
-		int result = src.getNumericId();
-		result = 31 * result + dst.getNumericId();
-		return result;
+			return (int) Math.signum(2 * Integer.compare(src().getNumericId(), o.src().getNumericId())
+					+ Integer.compare(dst().getNumericId(), o.dst().getNumericId()));
 	}
 }
