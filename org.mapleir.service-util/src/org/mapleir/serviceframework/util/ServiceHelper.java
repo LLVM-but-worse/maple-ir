d9e752cc617f31e59d7986dd3b454b9f1aef966d
Bibl_ <GenerallyCool@hotmail.com>
2017-09-28 23:45:43 +0100

    Add global IServiceContext to ServiceHelper
    The global context can be used to share services and service factories
    across different context layers. The design decision to use contexts in
    the service registry API and implementation was in hindsight a bit
    coarse as the context handling logic and framework could have been left
    to the client to handle on the application scope. Instead, an elegant
    solution is to have a context that is not able to be constrained or
    targeted specifically by the application layer.

--- org.mapleir.service-util/src/org/mapleir/serviceframework/util/ServiceHelper.java
+++ org.mapleir.service-util/src/org/mapleir/serviceframework/util/ServiceHelper.java
@@ -8,16 +8,22 @@ import org.mapleir.serviceframework.impl.ServiceRegistryImpl;
 public class ServiceHelper {
 	
 	private static IServiceRegistry GLOBAL_REGISTRY;
+	private static IServiceContext GLOBAL_CONTEXT;
 	
 	public static IServiceRegistry getGlobalServiceRegistry() {
 		return GLOBAL_REGISTRY;
 	}
 	
+	public static IServiceContext getGlobalServiceContext() {
+		return GLOBAL_CONTEXT;
+	}
+	
 	private static void __bootstrap() {
-		if(GLOBAL_REGISTRY != null) {
+		if(GLOBAL_REGISTRY != null || GLOBAL_CONTEXT != null) {
 			throw new RuntimeException(new IllegalStateException());
 		} else {
 			GLOBAL_REGISTRY = new ServiceRegistryImpl();
+			GLOBAL_CONTEXT = new GlobalServiceContext();
 		}
 	}
 	
