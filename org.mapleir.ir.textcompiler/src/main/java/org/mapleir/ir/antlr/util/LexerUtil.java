dcb5d4211f9b1a4d7418a03a3fa836b151921cc9
TheBiblMan <generallycool@hotmail.com>
2017-10-25 15:33:58 +0100

    Add LexerUtil
    Added LexerUtil utility class to convert internal lexer tokens to java
    objects or primitives.

---
+++ org.mapleir.ir.textcompiler/src/main/java/org/mapleir/ir/antlr/util/LexerUtil.java
@@ -0,0 +1,81 @@
+package org.mapleir.ir.antlr.util;
+
+import java.text.ParseException;
+
+import org.mapleir.ir.antlr.internallex.LexerException;
+import org.mapleir.ir.antlr.internallex.LiteralLexer;
+
+public class LexerUtil {
+
+	public static Object decodeNumericLiteral(String input) throws ParseException {
+		LiteralLexer lexer = new LiteralLexer(input.toCharArray());
+		
+		try {
+			lexer.process();
+		} catch(LexerException e) {
+			throw new ParseException(e.getMessage(), e.getBufferPointer());
+		}
+		
+		switch(lexer.getTokenKind()) {
+			case INTLIT: {
+				try {
+					return Conversion.asInt(lexer.asString(), lexer.getRadix());
+				} catch (NumberFormatException e) {
+					throw new ParseException(e.getMessage(), lexer.getBufferPointer());
+				}
+			}
+			case LONGLIT: {
+				try {
+					return Conversion.asLong(lexer.asString(), lexer.getRadix());
+				} catch (NumberFormatException e) {
+					throw new ParseException(e.getMessage(), lexer.getBufferPointer());
+				}
+			}
+			case FLOATLIT: {
+				String proper = lexer.getRadix() == 16 ? ("0x" + lexer.asString()) : lexer.asString();
+				
+				Float n;
+				
+				try {
+					n = Float.valueOf(proper);
+				} catch(NumberFormatException ex) {
+					 // should throw before we get to here
+					n = Float.NaN;
+				}
+				
+				if(n.floatValue() == 0.0f && !Conversion.isZero(proper)) {
+					throw new ParseException("fp number too small: " + proper, lexer.getBufferPointer());
+				} else if(n.floatValue() == Float.POSITIVE_INFINITY) {
+					throw new ParseException("fp number too large: " + proper, lexer.getBufferPointer());
+				}
+				
+				return n;
+			}
+			case DOUBLELIT: {
+				String proper = lexer.getRadix() == 16 ? ("0x" + lexer.asString()) : lexer.asString();
+				
+				Double n;
+	            try {
+	                n = Double.valueOf(proper);
+	            } catch (NumberFormatException ex) {
+	            	 // should throw before we get to here
+	                n = Double.NaN;
+	            }
+	            
+	            if(n.doubleValue() == 0.0d && !Conversion.isZero(proper)) {
+					throw new ParseException("fp number too small: " + proper, lexer.getBufferPointer());
+	            } else if(n.floatValue() == Float.POSITIVE_INFINITY) {
+					throw new ParseException("fp number too large: " + proper, lexer.getBufferPointer());
+				}
+	            
+	            return n;
+			}
+			case CHARLIT:
+				return lexer.asString().charAt(0);
+			case STRLIT:
+				return lexer.asString();
+			default:
+				throw new UnsupportedOperationException("Unknown token type: " + lexer.getTokenKind());
+		}
+	}
+}
