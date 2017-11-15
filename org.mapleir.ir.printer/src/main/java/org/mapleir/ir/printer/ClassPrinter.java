4622bd99d21ff2c965dd2a090ec3f2b498a160c1
TheBiblMan <generallycool@hotmail.com>
2017-11-15 12:31:25 +0000

    Add annotation support for ClassPrinter

--- org.mapleir.ir.printer/src/main/java/org/mapleir/ir/printer/ClassPrinter.java
+++ org.mapleir.ir.printer/src/main/java/org/mapleir/ir/printer/ClassPrinter.java
@@ -8,6 +8,8 @@ import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldNode;
 import org.objectweb.asm.tree.MethodNode;
 
+import static org.mapleir.ir.printer.Util.isNonEmpty;
+
 public class ClassPrinter extends ASMPrinter<ClassNode> {
 
     private static final String[] CLASS_ATTR_FLAG_NAMES = Util.asOpcodesAccessFieldFormat(
@@ -33,11 +35,39 @@ public class ClassPrinter extends ASMPrinter<ClassNode> {
         this.emitDirective("interfaces", e.interfaces);
         this.emitDirective("version", e.version);
         this.emitAccessDirective(e.access);
+        
+        this.emitIfNonNull("sourceFile", e.sourceFile);
+        this.emitIfNonNull("sourceFile", e.sourceDebug);
+        this.emitIfNonNull("outerClass", e.outerClass);
+        this.emitIfNonNull("outerMethod", e.outerMethod);
+        this.emitIfNonNull("outerMethodDesc", e.outerMethodDesc);
+        
+        if(isNonEmpty(e.visibleAnnotations)) {
+            this.emitDirective("visibleAnnotations", e.visibleAnnotations);
+        }
+        if(isNonEmpty(e.invisibleAnnotations)) {
+            this.emitDirective("invisibleAnnotations", e.invisibleAnnotations);
+        }
+        if(isNonEmpty(e.visibleTypeAnnotations)) {
+            this.emitDirective("visibleTypeAnnotations", e.visibleTypeAnnotations);
+        }
+        if(isNonEmpty(e.invisibleTypeAnnotations)) {
+            this.emitDirective("invisibleTypeAnnotations", e.invisibleTypeAnnotations);
+        }
+        if(isNonEmpty(e.attrs)) {
+            this.emitNodeAttributes(e.attrs);
+        }
 
         this.emit(e.fields.iterator(), this.fieldPrinter);
         this.emit(e.methods.iterator(), this.methodPrinter);
 
         this.sw.untab().newline().print("}");
+    }
+    
+    private void emitIfNonNull(String name, Object val) {
+        if(val != null) {
+            this.emitDirective(name, val);
+        }
     }
 
     private <E> void emit(Iterator<E> it, Printer<E> printer) {
