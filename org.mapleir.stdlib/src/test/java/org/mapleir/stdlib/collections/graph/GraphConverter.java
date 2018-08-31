8e1b29e086557f557409299359cffb08a6937c50
TheBiblMan <biblthebest@hotmail.com>
2018-09-01 00:38:49 +0100

    Start tests for pre and post dfs algorithms

---
+++ org.mapleir.stdlib/src/test/java/org/mapleir/stdlib/collections/graph/GraphConverter.java
@@ -0,0 +1,44 @@
+package org.mapleir.stdlib.collections.graph;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mapleir.dot4j.model.Edge;
+import org.mapleir.dot4j.model.Graph;
+import org.mapleir.dot4j.model.Node;
+import org.mapleir.dot4j.model.PortNode;
+import org.mapleir.dot4j.parse.Parser;
+
+public class GraphConverter {
+
+	public static FastGraph<OrderedNode, OrderedNode.ONEdge> fromFile(String name) throws IOException {
+		return fromDot(Parser.read(GraphConverter.class.getResourceAsStream(name)));
+	}
+	
+	public static FastGraph<OrderedNode, OrderedNode.ONEdge> fromDot(Graph graph) {		
+		FastGraph<OrderedNode, OrderedNode.ONEdge> fg;
+		if(graph.isDirected()) {
+			fg = new OrderedNode.ODirectedGraph();
+		} else {
+			fg = new OrderedNode.OUndirectedGraph();
+		}
+		Map<Node, OrderedNode> mapping = new HashMap<>();
+		for(Node node : graph.getAllNodes()) {
+			int id = Integer.parseInt(node.getName().toString());
+			OrderedNode oNode = new OrderedNode(id);
+			mapping.put(node, oNode);
+			fg.addVertex(oNode);
+		}
+		
+		for(Node node : graph.getAllNodes()) {
+			for(Edge e : node.getEdges()) {
+				OrderedNode src = mapping.get(node);
+				OrderedNode dst = mapping.get(((PortNode)e.getTarget()).getNode());
+				fg.addEdge(new OrderedNode.ONEdge(src, dst));
+			}
+		}
+		
+		return fg;
+	}
+}
