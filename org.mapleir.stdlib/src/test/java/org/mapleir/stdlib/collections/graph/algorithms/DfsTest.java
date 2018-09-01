a9db2a678eb97d9e098112ab7acd08febbbfbfe8
rollsafe <rollsafe@users.noreply.github.com>
2018-09-01 04:59:42 -0400

    Scrap SimpleDfs and just make it an interface to ExtendedDfs

--- org.mapleir.stdlib/src/test/java/org/mapleir/stdlib/collections/graph/algorithms/DfsTest.java
+++ org.mapleir.stdlib/src/test/java/org/mapleir/stdlib/collections/graph/algorithms/DfsTest.java
@@ -1,8 +1,12 @@
 package org.mapleir.stdlib.collections.graph.algorithms;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterators;
 import org.mapleir.stdlib.collections.graph.GraphConverter;
 import org.mapleir.stdlib.collections.graph.OrderedNode;
 import org.mapleir.stdlib.collections.graph.OrderedNode.ODirectedGraph;
@@ -13,31 +17,48 @@ import junit.framework.TestCase;
 
 public class DfsTest extends TestCase {
 
-	public void testSimpleDfs() throws IOException {
-		ODirectedGraph g = (ODirectedGraph) GraphConverter.fromFile("/dfspre1.gv");
-		SimpleDfs<OrderedNode> dfs = new SimpleDfs<OrderedNode>(g, getNode(g, 1), SimpleDfs.PRE | SimpleDfs.POST);
+	ODirectedGraph g;
+
+	@Override
+	public void setUp() {
+		try {
+			g = (ODirectedGraph) GraphConverter.fromFile("/dfspre1.gv");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testSimpleDfsPre() {
+		DepthFirstSearch<OrderedNode> dfs = new SimpleDfs<OrderedNode>(g, getNode(g, 1), SimpleDfs.PRE);
 		List<OrderedNode> res = dfs.getPreOrder();
-		assertOrdered(res, g.size());
+		assertTopoOrdered(res, g.size());
 	}
-	
-	public void testExtendedDfsPre1() throws IOException {
-		ODirectedGraph g = (ODirectedGraph) GraphConverter.fromFile("/dfspre1.gv");
-		ExtendedDfs<OrderedNode> dfs = new ExtendedDfs<>(g, ExtendedDfs.PRE).run(getNode(g, 1));
+
+	public void testExtendedDfsPre() {
+		DepthFirstSearch<OrderedNode> dfs = new ExtendedDfs<>(g, ExtendedDfs.PRE).run(getNode(g, 1));
 		List<OrderedNode> res = dfs.getPreOrder();
-		assertOrdered(res, g.size());
+		assertTopoOrdered(res, g.size());
 	}
-	
-	public void testExtendedDfsPost1() throws IOException {
-		ODirectedGraph g = (ODirectedGraph) GraphConverter.fromFile("/dfspost1.gv");
-		ExtendedDfs<OrderedNode> dfs = new ExtendedDfs<>(g, ExtendedDfs.POST).run(getNode(g, 9));
-		List<OrderedNode> res = dfs.getPostOrder();
-		assertOrdered(res, g.size());
+
+	public void testSimpleDfsTopo() {
+		DepthFirstSearch<OrderedNode> dfs = new SimpleDfs<OrderedNode>(g, getNode(g, 1), SimpleDfs.TOPO);
+		List<OrderedNode> res = dfs.getTopoOrder();
+		assertTopoOrdered(res, g.size());
 	}
-	
-	private static void assertOrdered(List<OrderedNode> nodes, int ex) {
+
+	public void testExtendedDfsTopo() {
+		DepthFirstSearch<OrderedNode> dfs = new ExtendedDfs<>(g, ExtendedDfs.TOPO).run(getNode(g, 1));
+		List<OrderedNode> res = dfs.getTopoOrder();
+		assertTopoOrdered(res, g.size());
+	}
+
+	private void assertTopoOrdered(List<OrderedNode> nodes, int ex) {
+		System.out.println(nodes);
+		Set<OrderedNode> visited = new HashSet<>();
 		assertEquals("missing nodes", ex, nodes.size());
-		for(int i=0; i < nodes.size(); i++) {
-			assertEquals(nodes.toString(), i + 1, nodes.get(i).time);
+		for (OrderedNode node : nodes) {
+			visited.add(node);
+			assertTrue("unvisited pred", Iterators.all(g.getPredecessors(node).iterator(), Predicates.in(visited)));
 		}
 	}
 	
