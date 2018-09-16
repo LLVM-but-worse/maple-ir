593a3fdbc30ef9466129064dde09296afa1cc54f
TheBiblMan <biblthebest@hotmail.com>
2018-09-16 16:04:50 +0100

    Replace TarjanDominanceComputor with LT79Dom

---
+++ org.mapleir.flowgraph/src/main/java/org/mapleir/flowgraph/algorithms/DominatorTree.java
@@ -0,0 +1,8 @@
+package org.mapleir.flowgraph.algorithms;
+
+import org.mapleir.stdlib.collections.graph.FastDirectedGraph;
+import org.mapleir.stdlib.collections.graph.FastGraphEdge;
+import org.mapleir.stdlib.collections.graph.FastGraphVertex;
+
+public class DominatorTree<N extends FastGraphVertex> extends FastDirectedGraph<N, FastGraphEdge<N>> {
+}
