137e77ac8e068a38ba0919e4d40960c42b8d5bc8
rollsafe <rollsafe@users.noreply.github.com>
2018-12-31 22:09:41 -0500

    IJavaDescUse interface

--- org.mapleir.stdlib/src/main/java/org/mapleir/stdlib/util/IJavaDescUse.java
+++
@@ -1,9 +0,0 @@
-package org.mapleir.stdlib.util;
-
-public interface IJavaDescUse extends IHasJavaDesc {
-    JavaDescUse.UseType getDataUseType();
-
-    default JavaDescUse getDataUse() {
-        return new JavaDescUse(getJavaDesc(), this, getDataUseType());
-    }
-}
