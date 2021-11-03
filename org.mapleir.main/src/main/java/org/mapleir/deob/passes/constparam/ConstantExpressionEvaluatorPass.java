d6ad7c95da25f7c2f40aa8d26da295848187dbe9
Thibaut Gautier <shanyujuneja@gmail.com>
2021-11-03 17:53:49 +0100

    The fact I have to make a new release for this makes me want to cry

--- org.mapleir.main/src/main/java/org/mapleir/deob/passes/constparam/ConstantExpressionEvaluatorPass.java
+++ org.mapleir.main/src/main/java/org/mapleir/deob/passes/constparam/ConstantExpressionEvaluatorPass.java
@@ -223,7 +223,7 @@ public class ConstantExpressionEvaluatorPass implements IPass, Opcode {
 	/**
 	 * Provides values of locals based on constant parameter information from call trace (IPConstAnalysisVisitor)
 	 */
-	private class SemiConstantLocalValueResolver implements LocalValueResolver {
+	public static class SemiConstantLocalValueResolver implements LocalValueResolver {
 		
 		private final IPConstAnalysisVisitor vis;
 		
@@ -271,7 +271,7 @@ public class ConstantExpressionEvaluatorPass implements IPass, Opcode {
 	/**
 	 * Stores information regarding constant parameters encountered during call tracing.
 	 */
-	public class IPConstAnalysisVisitor implements IPAnalysisVisitor {
+	public static class IPConstAnalysisVisitor implements IPAnalysisVisitor {
 		final AnalysisContext cxt;
 		final Map<ControlFlowGraph, List<TaintableSet<ConstantExpr>>> constParams = new HashMap<>();
 		
