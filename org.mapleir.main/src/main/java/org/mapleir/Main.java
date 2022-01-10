533e3cfe90e62fe10a2a02ff552be95291d3b593
Thibaut Gautier <shanyujuneja@gmail.com>
2022-01-10 20:12:25 +0100

    Added version and parenting command

--- org.mapleir.main/src/main/java/org/mapleir/Main.java
+++ org.mapleir.main/src/main/java/org/mapleir/Main.java
@@ -1,11 +1,18 @@
 package org.mapleir;
 
+import org.mapleir.cli.cmd.MainCommand;
 import org.mapleir.cli.cmd.RunCommand;
+import org.mapleir.cli.cmd.VersionCommand;
 import picocli.CommandLine;
 
 public class Main {
+    public static final String VERSION = Main.class.getPackage().getImplementationVersion();
+
     public static void main(final String[] args) {
-        int exitCode = new CommandLine(new RunCommand()).execute(args);
+        final int exitCode = new CommandLine(new MainCommand())
+                .addSubcommand("run", new RunCommand())
+                .addSubcommand("version", new VersionCommand())
+                .execute(args);
         System.exit(exitCode);
     }
 }
