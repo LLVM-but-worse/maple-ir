a5a4b4b620ee063f0de8fd7b8034be3587aa6ebf
Graham Kelly <gkgoat6700@gmail.com>
2025-01-18 14:27:36 +0000

    jitpack ripped from the source

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ rm -rf ./upstream ./org.mapleir.parent
 git clone https://github.com/skidfuscatordev/skidfuscator-java-obfuscator.git upstream
 cp -r ./upstream/org.mapleir.parent ./org.mapleir.parent
 cp ./upstream/build.gradle ./build.gradle
+cp ./upstream/jitpack.yml ./jitpack.yml
 cat ./upstream/settings.gradle | sed -e 's/skidfuscator-master/mapleir-main/g' | sed -e 's/void initBase()/void unused()/g' | sed -e 's/initBase()//g' > ./settings.gradle
