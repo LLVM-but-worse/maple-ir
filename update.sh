18a8dc1d925205204dcaca47b8b02a1f456e0b7e
Graham Kelly <gkgoat6700@gmail.com>
2025-04-19 00:53:11 +0000

    publish

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@ cd $(dirname $0)
 rm -rf ./upstream ./org.mapleir.parent
 git clone https://github.com/skidfuscatordev/skidfuscator-java-obfuscator.git upstream
 cp -r ./upstream/org.mapleir.parent ./org.mapleir.parent
-cp ./upstream/build.gradle ./build.gradle
+cat ./portal/pre.gradle > ./build.gradle
+cat ./upstream/build.gradle >> ./build.gradle
+cat ./portal/build2.gradle >> ./build.gradle
 cp ./upstream/jitpack.yml ./jitpack.yml
 cat ./upstream/settings.gradle | sed -e 's/skidfuscator-master/mapleir-main/g' | sed -e 's/void initBase()/void unused()/g' | sed -e 's/initBase()//g' > ./settings.gradle
